c866a7a68db75d2ec41ef29f483a22b9abcbae47
Caagr98 <Caagr98@users.noreply.github.com>
2019-12-10 20:18:51 +0100

    Give the solution properly

--- 19/8.py
+++ 19/8.py
@@ -1,8 +1,8 @@
 import numpy as np
 layers = np.array([int(a) for a in input()]).reshape(-1, 6, 25)
 
-for l in layers:
-	print((l==0).sum(), (l==1).sum() * (l==2).sum())
+l = layers[(layers==0).sum(axis=(1,2)).argmin()]
+print((l==1).sum() * (l==2).sum())
 
 m = np.full_like(layers[0], 2)
 for l in layers:
