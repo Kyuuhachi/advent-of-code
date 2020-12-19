1848020e559f346bdfb3eceab48dca5f06df46c0
Kyuuhachi <caagr98@gmail.com>
2020-12-19 10:32:14 +0100

    Day 19
    Doesn't actually solve the complete task for arbitrary inputs.

---
+++ 20/19.py
@@ -0,0 +1,27 @@
+import lark
+[a, b] = open("19.in").read().split("\n\n")
+rules = {}
+for r in a.splitlines():
+	k, v = r.split(": ")
+	if '"' in v:
+		rules[int(k)] = v[1:-1]
+	else:
+		rules[int(k)] = tuple(tuple(int(w) for w in a.split(" ")) for a in v.split(" | "))
+lines = b.splitlines()
+
+import re
+class a(dict):
+	def __missing__(f, key):
+		v = rules[key]
+		if isinstance(v, str):
+			return v
+		else:
+			return "(%s)" % "|".join("".join(f[a] for a in b) for b in v)
+r = re.compile(a()[0])
+print(sum(bool(r.fullmatch(s)) for s in lines))
+
+w = a()
+w[8] = f"({w[42]}+)"
+w[11] = "(%s)" % "|".join(w[42]*n+w[31]*n for n in range(1, 8))
+r = re.compile(w[0])
+print(sum(bool(r.fullmatch(s)) for s in lines))
