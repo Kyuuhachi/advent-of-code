01ec5c0f07d04741bf7935df49eee8dfc862ec1a
Kyuuhachi <caagr98@gmail.com>
2021-12-02 08:24:46 +0100

    Merge parts 1 and 2

--- 21/02.py
+++ 21/02.py
@@ -1,15 +1,8 @@
 lines = [(a, int(b)) for l in open("02.in") for (a, b) in [l.split()]]
 
-x, y = 0, 0
+x, y, z = 0, 0, 0
 for a, b in lines:
-	if a == "forward": x += b
+	if a == "forward": x += b; z += y * b
 	if a == "up": y -= b
 	if a == "down": y += b
-print(x*y)
-
-x, y, aim = 0, 0, 0
-for a, b in lines:
-	if a == "forward": x += b; y += aim * b
-	if a == "up": aim -= b
-	if a == "down": aim += b
-print(x*y)
+print(x*y, x*z)
