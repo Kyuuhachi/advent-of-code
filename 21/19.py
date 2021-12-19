9fc074528a8f8278ab7b8f4deeaf933b2f2682f3
Kyuuhachi <caagr98@gmail.com>
2021-12-19 12:37:14 +0100

    Make it significantly faster
    From 2:55 to 1:15. Still way too slow, mind you

--- 21/19.py
+++ 21/19.py
@@ -9,7 +9,7 @@ for line in open("19.in"):
 	else:
 		inp[-1].append(tuple(int(a) for a in line.split(",")))
 
-inp = [np.array(x) for x in inp]
+inp = [np.array(x, dtype="i8") for x in inp]
 
 def orient(a):
 	for b in [
@@ -25,21 +25,21 @@ def orient(a):
 		yield -b[:,[2,1,0]]
 		yield -b[:,[1,0,2]]
 
-def sort(a):
-	return np.array(sorted(a.tolist()))
+def hash_(a):
+	return a[:,0] * 1000000_000000 + a[:,1] * 1000000 + a[:,2]
 
 def align(b, a):
-	sb = {tuple(x) for x in b}
-	for a in map(sort, orient(a)):
+	sb = hash_(b)
+	for a in orient(a):
 		for ra in a:
 			for rb in b:
-				sa = {tuple(x) for x in a+rb-ra}
-				if len(sa & sb) >= 12:
+				sa = hash_(a+rb-ra)
+				if len(np.intersect1d(sa, sb, assume_unique=True)) >= 12:
 					return a+rb-ra, rb-ra
 
 inp2 = list(inp)
 done = []
-done.append(sort(inp2.pop(0)))
+done.append(inp2.pop(0))
 for a in done:
 	for i, b in reversed(list(enumerate(inp2))):
 		match align(a, b):
