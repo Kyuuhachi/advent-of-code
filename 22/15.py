d54413ead2e7b4271abe921f170f77750844f5b7
Kyuuhachi <caagr98@gmail.com>
2022-12-15 17:31:41 +0100

    Faster 15b
    From 27s to 30ms. May not work if there are multiple holes, especially
    adjacent ones, but for singular holes it works fine.

--- 22/15.py
+++ 22/15.py
@@ -4,24 +4,18 @@ rx = re.compile(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?i?\d
 # different limits for prod and test
 TARGET_LINE = 10
 TARGET_LINE = 2000000
-SIZE = TARGET_LINE*2+1
 
 inp = []
 for line in open("15.in"):
-	a,b,c,d = rx.fullmatch(line.strip()).groups()
-	inp.append(((int(a),int(b)),(int(c),int(d))))
+	a,b,c,d = map(int, rx.fullmatch(line.strip()).groups())
+	inp.append((a,b,abs(a-c)+abs(b-d)))
 
-def _row(n):
-	row = []
-	for (a,b),(c,d) in inp:
-		distToLine = abs(b-n)
-		radius = abs(a-c)+abs(b-d)
-		if radius >= distToLine:
-			row.append((a-(radius-distToLine),a+(radius-distToLine)))
-	row.sort()
-	return row
-
-row = _row(TARGET_LINE)
+row = []
+for a,b,r in inp:
+	distToLine = abs(b-TARGET_LINE)
+	if r >= distToLine:
+		row.append((a-(r-distToLine),a+(r-distToLine)))
+row.sort()
 end = row[0][0]
 n = 0
 for a, b in row:
@@ -30,15 +24,11 @@ for a, b in row:
 		end=b
 print(n)
 
-for i in range(2630000, 2640000):
-	if i % 10000 == 0:
-		print(i)
-	row = _row(i)
-	if row:
-		end = 0
-		for a, b in row:
-			if end < a-1:
-				print(i+(a-1)*4000000)
-			end = max(b, end)
-
-
+p1 = {a+b-(r+1) for a,b,r in inp} & {a+b+(r+1) for a,b,r in inp}
+p2 = {a-b-(r+1) for a,b,r in inp} & {a-b+(r+1) for a,b,r in inp}
+print({
+	a*4000000+b
+	for q1 in p1 for q2 in p2
+	for (a,b) in [((q1+q2)//2, (q1-q2)//2)]
+	if all(abs(a-c)+abs(b-d) > r for c,d,r in inp)
+})
