37bd262e36550b050b839c42751d361fc0db6d87
Kyuuhachi <caagr98@gmail.com>
2022-12-18 12:46:13 +0100

    Day 18
    Easy peasy.

---
+++ 22/18.py
@@ -0,0 +1,31 @@
+import numpy as np
+x = np.zeros((24,24,24), dtype=bool)
+for l in open("18.in"):
+	a, b, c = map(int, l.strip().split(","))
+	x[a+1,b+1,c+1] = True
+
+n = 0
+for a in 0,1,2:
+	for b in -1,1:
+		n += (x & ~np.roll(x, b, axis=a)).sum()
+print(n)
+
+y = np.zeros_like(x)
+y[[0,-1],:,:] = True
+y[:,[0,-1],:] = True
+y[:,:,[0,-1]] = True
+assert not np.any(x&y)
+while True:
+	p = y.sum()
+	for a in 0,1,2:
+		for b in -1,1:
+			y |= np.roll(y, b, axis=a) & ~x
+	if y.sum() == p:
+		break
+
+x = ~y
+n = 0
+for a in 0,1,2:
+	for b in -1,1:
+		n += (x & ~np.roll(x, b, axis=a)).sum()
+print(n)
