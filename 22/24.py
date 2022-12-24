1fd26020d08734be4968c04ce9cab9165cc0d146
Kyuuhachi <caagr98@gmail.com>
2022-12-24 16:08:29 +0100

    Improve 24

--- 22/24.py
+++ 22/24.py
@@ -23,40 +23,31 @@ bliz[:,-1,-2] = False
 d = np.zeros((3, *bliz.shape), dtype="u2")
 d[:, bliz] = -1
 
-d[0, 0,0,1] = 1
-q = [(0, 0,0,1)]
+d[0,0,0,1] = 1
+q = [(0,0,0,1)]
 for k,t,y,x in q:
-	T = (t + 1) % len(bliz)
+	w = [(y,x)]
 	if y == 0:
+		w.append((y+1,x))
 		if k == 1:
 			d[2,t,y,x] = d[1,t,y,x]
 			k = 2
-		if not d[k,T,y+1,x]:
-			d[k,T,y+1,x] = d[k,t,y,x]+1
-			q.append((k,T,y+1,x))
 	elif y == bliz.shape[-2]-1:
+		w.append((y-1,x))
 		if k == 0:
 			d[1,t,y,x] = d[0,t,y,x]
 			k = 1
-		if not d[k,T,y-1,x]:
-			d[k,T,y-1,x] = d[k,t,y,x]+1
-			q.append((k,T,y-1,x))
 	else:
-		if not d[k,T,y-1,x]:
-			d[k,T,y-1,x] = d[k,t,y,x]+1
-			q.append((k,T,y-1,x))
-		if not d[k,T,y,x-1]:
-			d[k,T,y,x-1] = d[k,t,y,x]+1
-			q.append((k,T,y,x-1))
-		if not d[k,T,y+1,x]:
-			d[k,T,y+1,x] = d[k,t,y,x]+1
-			q.append((k,T,y+1,x))
-		if not d[k,T,y,x+1]:
-			d[k,T,y,x+1] = d[k,t,y,x]+1
-			q.append((k,T,y,x+1))
-	if not d[k,T,y,x]:
-		d[k,T,y,x] = d[k,t,y,x]+1
-		q.append((k,T,y,x))
+		w.append((y-1,x))
+		w.append((y+1,x))
+		w.append((y,x-1))
+		w.append((y,x+1))
+
+	T = (t + 1) % len(bliz)
+	for y0,x0 in w:
+		if not d[k,T,y0,x0]:
+			d[k,T,y0,x0] = d[k,t,y,x]+1
+			q.append((k,T,y0,x0))
 
 e = d[0,:,-1,-2]
 print((e-1).min())
