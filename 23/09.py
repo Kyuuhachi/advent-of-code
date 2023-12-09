b8bef7822064827c573511cf85a2c8bf063cc5a6
Kyuuhachi <caagr98@gmail.com>
2023-12-09 09:27:44 +0100

    Shorten even more

--- 23/09.py
+++ 23/09.py
@@ -2,7 +2,6 @@ import numpy as np
 input = [np.array([int(a) for a in line.split()]) for line in open("09.in")]
 
 def calc(v):
-	w = v if np.all(v == 0) else calc(np.diff(v))
-	return np.concatenate([[v[0]], w]).cumsum()
-print(sum(calc(v)[-1] for v in input))
-print(sum(calc(v[::-1])[-1] for v in input))
+	return 0 if np.all(v == 0) else calc(np.diff(v)) + v[-1]
+print(sum(calc(v) for v in input))
+print(sum(calc(v[::-1]) for v in input))
