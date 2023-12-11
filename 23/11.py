3d00b2037553d1269345885cf78e3149d454f7c0
Kyuuhachi <caagr98@gmail.com>
2023-12-11 10:14:22 +0100

    No more rolls

--- 23/11.py
+++ 23/11.py
@@ -2,12 +2,10 @@ import numpy as np
 input = np.array([list(i) for i in open("11.in").read().splitlines()])
 
 v = np.sum(input == '#', axis=1)
-w = np.cumsum(v) * np.roll(np.cumsum(v[::-1])[::-1], -1)
-w[-1] = 0
+w = np.cumsum(v[:-1]) * np.cumsum(v[:0:-1])[::-1]
 
 h = np.sum(input == '#', axis=0)
-z = np.cumsum(h) * np.roll(np.cumsum(h[::-1])[::-1], -1)
-z[-1] = 0
+z = np.cumsum(h[:-1]) * np.cumsum(h[:0:-1])[::-1]
 
 g = np.array([[1, 1], [2, 1000000]])
-print(w @ g[(v == 0)+0] + z @ g[(h == 0)+0])
+print(w @ g[(v[:-1] == 0)+0] + z @ g[(h[:-1] == 0)+0])
