49e364d8095bf35d15f36253a1d52ea0c1904159
Kyuuhachi <caagr98@gmail.com>
2023-12-11 09:56:37 +0100

    Now O(n)

--- 23/11.py
+++ 23/11.py
@@ -1,17 +1,13 @@
 import numpy as np
 input = np.array([list(i) for i in open("11.in").read().splitlines()])
 
-v = np.all(input == '.', axis=1)
-h = np.all(input == '.', axis=0)
-sx, sy = np.nonzero(input == '#')
+v = np.sum(input == '#', axis=1)
+w = np.cumsum(v) * np.roll(np.cumsum(v[::-1])[::-1], -1)
+w[-1] = 0
 
-w = np.zeros_like(input[0,:], dtype="u8")
-for a in sx:
-	for b in sx:
-		w[a:b] += 1
-z = np.zeros_like(input[0,:], dtype="u8")
-for a in sy:
-	for b in sy:
-		z[a:b] += 1
-print((w + w*v + z + z*h).sum(dtype="u8"))
-print((w + w*v*999999 + z + z*h*999999).sum(dtype="u8"))
+h = np.sum(input == '#', axis=0)
+z = np.cumsum(h) * np.roll(np.cumsum(h[::-1])[::-1], -1)
+z[-1] = 0
+
+print((w + w*(v==0) + z + z*(h==0)).sum(dtype="u8"))
+print((w + w*(v==0)*999999 + z + z*(h==0)*999999).sum(dtype="u8"))
