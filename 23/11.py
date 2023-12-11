4a312cf94ee7c04807bfeacc4b8a68d2f1f5059a
Kyuuhachi <caagr98@gmail.com>
2023-12-11 10:16:07 +0100

    Abstract into function

--- 23/11.py
+++ 23/11.py
@@ -1,11 +1,10 @@
 import numpy as np
 input = np.array([list(i) for i in open("11.in").read().splitlines()])
 
-v = np.sum(input == '#', axis=1)
-w = np.cumsum(v[:-1]) * np.cumsum(v[:0:-1])[::-1]
-
-h = np.sum(input == '#', axis=0)
-z = np.cumsum(h[:-1]) * np.cumsum(h[:0:-1])[::-1]
-
 g = np.array([[1, 1], [2, 1000000]])
-print(w @ g[(v[:-1] == 0)+0] + z @ g[(h[:-1] == 0)+0])
+def f(a):
+	v = np.sum(input == '#', axis=a)
+	w = np.cumsum(v[:-1]) * np.cumsum(v[:0:-1])[::-1]
+	return w @ g[(v[:-1] == 0)+0]
+
+print(f(0) + f(1))
