754285b222ecf74cd20065d84dc8ef744019eb98
Kyuuhachi <caagr98@gmail.com>
2023-12-11 10:19:58 +0100

    Use inline cumsum

--- 23/11.py
+++ 23/11.py
@@ -4,7 +4,6 @@ input = np.array([list(i) for i in open("11.in").read().splitlines()])
 g = np.array([[1, 1], [2, 1000000]])
 def f(a):
 	v = np.sum(input == '#', axis=a)
-	w = np.cumsum(v[:-1]) * np.cumsum(v[:0:-1])[::-1]
+	w = v[:-1].cumsum() * v[:0:-1].cumsum()[::-1]
 	return w @ g[(v[:-1] == 0)+0]
-
 print(f(0) + f(1))
