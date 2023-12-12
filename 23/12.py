46211790532abc34b3e5b5b5a2fc1b7223afd56e
Kyuuhachi <caagr98@gmail.com>
2023-12-12 11:01:26 +0100

    Avoid some awkward bounds checks

--- 23/12.py
+++ 23/12.py
@@ -4,17 +4,18 @@ for i in open("12.in"):
 	input.append((a, [int(b) for b in b.split(",")]))
 
 def count(a: str, b: list[int]):
+	a += '.'
 	import functools
 	@functools.lru_cache(None)
 	def rec(i: int, j: int):
-		if i >= len(a): return j == len(b)
+		if i == len(a): return j == len(b)
 		if j == len(b): return '#' not in a[i:]
 		n = 0
 		if a[i] != '.':
 			k = i+b[j]
 			if len(a) >= k \
 					and '.' not in a[i:k] \
-					and not a[k:].startswith('#'):
+					and a[k] != '#':
 				n += rec(k+1, j+1)
 		if a[i] != '#':
 			n += rec(i+1, j)
