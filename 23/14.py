310a0ec7fd8d2998c8b239370e0c94ad9cafa553
Kyuuhachi <caagr98@gmail.com>
2023-12-14 11:51:53 +0100

    Use bool arrays, not string
    Halves runtime

--- 23/14.py
+++ 23/14.py
@@ -3,28 +3,31 @@ import numpy as np
 input = np.array([list(i) for i in open("14.in").read().splitlines()])
 input = np.pad(input, 1, constant_values='#')
 
-def tilt(grid):
+w = input == '#'
+def tilt(o, w):
 	while True:
-		a = grid == 'O'
-		b = np.roll(grid, 1, axis=0) == '.'
+		a = o
+		b = np.roll(~o & ~w, 1, axis=0)
 		k = a & b
 		if not k.sum(): break
-		grid[k] = '.'
-		grid[np.roll(k, -1, axis=0)] = 'O'
-	return grid
+		o[k] = False
+		o[np.roll(k, -1, axis=0)] = True
+	return o
 
 def score(grid):
-	return (np.argwhere(grid[::-1] == 'O')[:,0]).sum()
+	return (np.argwhere(grid[::-1])[:,0]).sum()
 
-print(score(tilt(np.copy(input))))
+print(score(tilt(input == 'O', input == '#')))
 
-g = np.copy(input)
+g = input == 'O'
+w = input == '#'
 h = {}
 for i in range(1,1000000):
 	for j in range(4):
-		g = tilt(g)
+		g = tilt(g, w)
 		g = np.rot90(g, -1)
-	bits = np.packbits(g == 'O').tobytes()
+		w = np.rot90(w, -1)
+	bits = np.packbits(g).tobytes()
 	if bits in h:
 		period = i - h[bits]
 		if i % period == 1000000000 % period:
