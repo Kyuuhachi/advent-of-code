f4a6bad9d72f17a2a0643b6ff59fee334ff3af84
Kyuuhachi <caagr98@gmail.com>
2023-12-19 11:17:37 +0100

    No varargs

--- 23/19.py
+++ 23/19.py
@@ -11,37 +11,35 @@ for line in a.splitlines():
 		then.append((p[0], p[1], int(p[2:]), q))
 	scheme[name] = (then, end)
 
-def test(what, **vars):
+def test(what, vars):
 	if what == "A": return True
 	if what == "R": return False
 
 	p, end = scheme[what]
-	for var, op, n, then in p:
-		if op == "<" and vars[var] < n: return test(then, **vars)
-		if op == ">" and vars[var] > n: return test(then, **vars)
-	return test(end, **vars)
+	for v, op, n, then in p:
+		if op == "<" and vars[v] < n: return test(then, vars)
+		if op == ">" and vars[v] > n: return test(then, vars)
+	return test(end, vars)
 
 n = 0
 for line in b.splitlines():
 	vars = {}
 	exec(line.strip()[1:-1].replace(",", ";"), {}, vars)
-	if test("in", **vars):
+	if test("in", vars):
 		n += sum(vars.values())
 print(n)
 
-def parse(what, **vars):
+def parse(what, vars):
 	from math import prod
 	if what == "A": return prod(a[1]-a[0]+1 for a in vars.values())
 	if what == "R": return 0
 
 	k = 0
 	p, end = scheme[what]
-	for var, op, n, then in p:
-		mn, mx = vars[var]
-		if op == "<": v1, v2 = (mn, n-1), (n, mx)
-		if op == ">": v1, v2 = (n+1, mx), (mn, n)
-		vars[var] = v1
-		k += parse(then, **vars)
-		vars[var] = v2
-	return k + parse(end, **vars)
-print(parse("in", x=(1,4000), m=(1,4000), a=(1, 4000), s=(1,4000)))
+	for v, op, n, then in p:
+		mn, mx = vars[v]
+		if op == "<": val, vars[v] = (mn, n-1), (n, mx)
+		if op == ">": val, vars[v] = (n+1, mx), (mn, n)
+		k += parse(then, vars | {v: val})
+	return k + parse(end, vars)
+print(parse("in", {k: (1,4000) for k in "xmas"}))
