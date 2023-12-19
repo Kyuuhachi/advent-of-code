e4b85ed2dbdd55c5773456a44942360c00b4fe86
Kyuuhachi <caagr98@gmail.com>
2023-12-19 11:11:46 +0100

    Less matching, more dict

--- 23/19.py
+++ 23/19.py
@@ -16,25 +16,21 @@ def cond(r, o, n):
 	if o == ">": return r > n
 	raise
 
-def test(what, x,m,a,s):
+def test(what, **vars):
 	if what == "A": return True
 	if what == "R": return False
 
 	p, end = scheme[what]
 	for var, op, n, then in p:
-		match var:
-			case "x" if cond(x, op, n): return test(then, x, m, a, s)
-			case "m" if cond(m, op, n): return test(then, x, m, a, s)
-			case "a" if cond(a, op, n): return test(then, x, m, a, s)
-			case "s" if cond(s, op, n): return test(then, x, m, a, s)
-	return test(end, x, m, a, s)
+		if cond(vars[var], op, n): return test(then, **vars)
+	return test(end, **vars)
 
 n = 0
 for line in b.splitlines():
-	v = {}
-	exec(line.strip()[1:-1].replace(",", ";"), v, v)
-	if test("in", v["x"], v["m"], v["a"], v["s"]):
-		n += v["x"] + v["m"] + v["a"] + v["s"]
+	vars = {}
+	exec(line.strip()[1:-1].replace(",", ";"), {}, vars)
+	if test("in", **vars):
+		n += sum(vars.values())
 print(n)
 
 def split(r, o, n):
@@ -42,17 +38,17 @@ def split(r, o, n):
 	if o == ">": return (n+1, r[1]), (r[0], n)
 	raise
 
-def parse(what, x,m,a,s):
-	if what == "A": return (x[1]-x[0]+1) * (m[1]-m[0]+1) * (a[1]-a[0]+1) * (s[1]-s[0]+1)
+def parse(what, **vars):
+	from math import prod
+	if what == "A": return prod(a[1]-a[0]+1 for a in vars.values())
 	if what == "R": return 0
 
 	k = 0
 	p, end = scheme[what]
 	for var, op, n, then in p:
-		match var:
-			case "x": q,x = split(x, op, n); k += parse(then, q, m, a, s)
-			case "m": q,m = split(m, op, n); k += parse(then, x, q, a, s)
-			case "a": q,a = split(a, op, n); k += parse(then, x, m, q, s)
-			case "s": q,s = split(s, op, n); k += parse(then, x, m, a, q)
-	return k + parse(end, x, m, a, s)
-print(parse("in", (1,4000), (1,4000), (1, 4000), (1,4000)))
+		v1, v2 = split(vars[var], op, n)
+		vars[var] = v1
+		k += parse(then, **vars)
+		vars[var] = v2
+	return k + parse(end, **vars)
+print(parse("in", x=(1,4000), m=(1,4000), a=(1, 4000), s=(1,4000)))
