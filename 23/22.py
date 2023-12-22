9d70445c9dec2b8cd38113b6f563fbd3ca8c7192
Kyuuhachi <caagr98@gmail.com>
2023-12-22 12:45:05 +0100

    Misc small optimizations

--- 23/22.py
+++ 23/22.py
@@ -10,17 +10,17 @@ bricks.sort(key=lambda a: a[2])
 
 height = np.zeros((10, 10), dtype="u4")
 top = np.full((10, 10), -1, dtype="i4")
-supported_by = {}
+supported_by = []
 for i,(x,z,y) in enumerate(bricks):
 	s = np.s_[x[0]:x[1]+1,z[0]:z[1]+1]
 	h = height[s].max()
-	supported_by[i] = set(np.unique(top[s][(height[s] == h)]))
-	assert h <= y[0], (h, (x,z,y,i))
+	supported_by.append(set(np.unique(top[s][(height[s] == h)])))
+	assert h <= y[0]
 	height[s] = h + y[1] + 1 - y[0]
 	top[s] = i
 
 can = set(range(len(bricks)))
-for sup in supported_by.values():
+for sup in supported_by:
 	if len(sup) == 1:
 		can -= sup
 print(len(can))
@@ -29,7 +29,7 @@ n = 0
 for i in range(len(bricks)):
 	broken = {i}
 	for j in range(i, len(bricks)):
-		if not (supported_by[j] - broken):
+		if supported_by[j] <= broken:
 			broken.add(j)
 	n += len(broken) - 1
 print(n)
