1c2e8cd91e58a9b814d4576f3bbf016f52541d6c
Kyuuhachi <caagr98@gmail.com>
2024-12-01 19:30:59 +0100

    Golf parsing
    Two bytes longer, but allows saving a lot on the sorting part

--- 24/01.golf.py
+++ 24/01.golf.py
@@ -1,3 +1,3 @@
-x=list(map(int,open("01.txt").read().split()));a=x[::2];b=x[1::2]
-s=sorted;print(sum(abs(q-w)for q,w in zip(s(a),s(b))))
+a,b=[sorted(int(x.split()[i]) for x in open("01.txt"))for i in(0,1)]
+print(sum(abs(q-w)for q,w in zip(a,b)))
 print(sum(q*b.count(q)for q in a))
