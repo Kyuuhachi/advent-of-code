7df38bd01a27d766e73631d5e63779d1b48f41e0
Kyuuhachi <caagr98@gmail.com>
2024-12-01 19:28:13 +0100

    Use .count

--- 24/01.golf.py
+++ 24/01.golf.py
@@ -1,3 +1,3 @@
 x=list(map(int,open("01.txt").read().split()));a=x[::2];b=x[1::2]
 s=sorted;print(sum(abs(q-w)for q,w in zip(s(a),s(b))))
-print(sum(q*(q==w)for q in a for w in b))
+print(sum(q*b.count(q)for q in a))
