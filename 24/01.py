994cfafb82a9139d7e9ebbb36a619b7db2c4bda2
Kyuuhachi <caagr98@gmail.com>
2024-12-01 11:22:01 +0100

    Day 1

---
+++ 24/01.py
@@ -0,0 +1,10 @@
+from collections import Counter
+a, b = [], []
+for line in open("01.txt").read().splitlines():
+	v1, v2 = line.split()
+	a.append(int(v1))
+	b.append(int(v2))
+
+print(sum(abs(v1 - v2) for v1, v2 in zip(sorted(a), sorted(b))))
+counts = Counter(b)
+print(sum(v1 * counts[v1] for v1 in a))
