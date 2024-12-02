f3a18bf5a3d988ecd3bff85590c2495da0eda267
Kyuuhachi <caagr98@gmail.com>
2024-12-02 22:37:26 +0100

    Shave off two bytes

--- 24/02b.py
+++ 24/02b.py
@@ -1,7 +1,5 @@
 def p(n,*x,c=0):
- for y in x:
-  if 0<y-n<4:n=y
-  else:c+=1
+ for y in x:k=0<y-n<4;c+=1-k;n=[n,y][k]
  return c
 a=b=0
 for l in open("02.in"):w=list(map(int,l.split()));k=min(p(*w),p(*w[::-1]));a+=k<1;b+=k<2
