67e8936dca38a1108c69c9961b6f606a70697c2b
Kyuuhachi <caagr98@gmail.com>
2024-12-02 21:09:20 +0100

    Save a few bytes by printing both on the same line

--- 24/01.golf.py
+++ 24/01.golf.py
@@ -1,3 +1,2 @@
 a,b=[sorted(int(x.split()[i]) for x in open("01.in"))for i in(0,1)]
-print(sum(abs(q-w)for q,w in zip(a,b)))
-print(sum(q*b.count(q)for q in a))
+print(sum(abs(q-w)for q,w in zip(a,b)),sum(q*b.count(q)for q in a))
