ef0280ee849a294176b26239aa6228573fdf91b3
Kyuuhachi <caagr98@gmail.com>
2024-12-02 21:11:19 +0100

    Golf day 1 further by mangling output format

--- 24/01.golf.py
+++ 24/01.golf.py
@@ -1,2 +1,2 @@
-a,b=[sorted(int(x.split()[i]) for x in open("01.in"))for i in(0,1)]
-print(sum(abs(q-w)for q,w in zip(a,b)),sum(q*b.count(q)for q in a))
+a,b=[sorted(int(x.split()[i])for x in open("01.in"))for i in(0,1)]
+print(sum(abs(q-w)+1j*q*b.count(q)for q,w in zip(a,b)))
