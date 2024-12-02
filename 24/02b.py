218f1a145540ea7f07c7e57d3441ac35d3c3f648
Kyuuhachi <caagr98@gmail.com>
2024-12-02 22:46:11 +0100

    Found trick that saves 4 bytes

--- 24/02b.py
+++ 24/02b.py
@@ -2,5 +2,5 @@ def p(n,*x,c=0):
  for y in x:k=0<y-n<4;c+=1-k;n=[n,y][k]
  return c
 a=b=0
-for l in open("02.in"):w=list(map(int,l.split()));k=min(p(*w),p(*w[::-1]));a+=k<1;b+=k<2
+for l in open("02.in"):*w,=map(int,l.split());k=min(p(*w),p(*w[::-1]));a+=k<1;b+=k<2
 print(a,b)
