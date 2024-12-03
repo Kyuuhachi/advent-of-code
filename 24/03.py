e426b9ceed53aaa284acb1f333f975644479c4c6
Kyuuhachi <caagr98@gmail.com>
2024-12-03 18:18:49 +0100

    179→171

--- 24/03.py
+++ 24/03.py
@@ -3,5 +3,5 @@ A=B=0
 w=1
 for a,b,c,d in re.findall(r"mul\((\d+),(\d+)\)|(d)on't\(\)|(d)o\(\)",open("03.in").read()):
  if c+d:w=c<d
- if a:A+=int(a)*int(b);B+=int(a)*int(b)*w
+ if a:h=int(a)*int(b);A+=h;B+=h*w
 print(A,B)
