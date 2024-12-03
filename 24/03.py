32c1631115b2dd63206da03245001d3aceac3bf8
Kyuuhachi <caagr98@gmail.com>
2024-12-03 19:00:50 +0100

    160→159

--- 24/03.py
+++ 24/03.py
@@ -1,7 +1,6 @@
 import re
-A=B=0
-w=1
+A=B=w=0
 for a,b,d in re.findall(r"mul\((\d+),(\d+)\)|do(n't)?\(\)",open("03.in").read()):
- if a:h=int(a)*int(b);A+=h;B+=h*w
- else:w=a>=d
+ if a:h=int(a)*int(b);A+=h;B-=h*~-w
+ else:w=a<d
 print(A,B)
