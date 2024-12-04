4b33fa1be55108749af96bcc56b74c32e72e664e
Kyuuhachi <caagr98@gmail.com>
2024-12-04 18:23:37 +0100

    237→229

--- 24/04.py
+++ 24/04.py
@@ -2,8 +2,9 @@ s=open("04.in").read()
 w=s.find("\n")+1
 c=lambda j:s[i::j][:4]=="XMAS"
 d=lambda j:s[i-j::j][:3]=="MAS"
+q=lambda f,j:f(j)+f(-j)
 n=m=0
 for i in range(len(s)):
- n+=c(1)+c(-1)+c(w)+c(-w)+c(w+1)+c(w-1)+c(-w+1)+c(-w-1)
- m+=d(w+1)+d(-w-1)and d(w-1)+d(-w+1)
+ n+=q(c,1)+q(c,w)+q(c,w+1)+q(c,w-1)
+ m+=q(d,w+1)and q(d,w-1)
 print(n,m)
