7ac8474fa59897c32be6503e56ba0762e6933795
Kyuuhachi <caagr98@gmail.com>
2024-12-04 18:48:15 +0100

    209

--- 24/04.py
+++ 24/04.py
@@ -3,5 +3,5 @@ q=lambda j:f(j)+f(-j)
 n=m=0
 for i in range(len(s)):
  f=lambda j:s[i::j][:4]=="XMAS";n+=q(1)+q(w:=s.find("\n")+1)+q(~w)+q(w-1)
- f=lambda j:s[i-j::j][:3]=="MAS";m+=q(~w)and q(w-1)
+ f=lambda j:s[i-j::j][:3]=="MAS";m+=q(~w)*q(w-1)
 print(n,m)
