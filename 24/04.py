b9e8c809877c903843773eecb1e28403cc454f7b
Kyuuhachi <caagr98@gmail.com>
2024-12-04 18:21:51 +0100

    238→237

--- 24/04.py
+++ 24/04.py
@@ -5,5 +5,5 @@ d=lambda j:s[i-j::j][:3]=="MAS"
 n=m=0
 for i in range(len(s)):
  n+=c(1)+c(-1)+c(w)+c(-w)+c(w+1)+c(w-1)+c(-w+1)+c(-w-1)
- m+=(d(w+1)|d(-w-1))&(d(w-1)|d(-w+1))
+ m+=d(w+1)+d(-w-1)and d(w-1)+d(-w+1)
 print(n,m)
