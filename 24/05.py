66d2909cc9b13412d3e65a886428179e9f6a1343
Kyuuhachi <caagr98@gmail.com>
2024-12-05 22:20:22 +0100

    260, why did I have that second loop

--- 24/05.py
+++ 24/05.py
@@ -7,10 +7,9 @@ for b in S(b):
  x=S(b,",");c=1;d=0
  while c:
   c=0
-  for a in range(l:=len(x)):
-   for b in range(a+1,l):
-    if(x[a+1],x[a])in p:
-     x[a],x[a+1]=x[a+1],x[a]
-     c=d=1
+  for a in range(l:=len(x)-1):
+   if(x[a+1],x[a])in p:
+    x[a],x[a+1]=x[a+1],x[a]
+    c=d=1
  A[d]+=int(x[l//2])
 print(*A)
