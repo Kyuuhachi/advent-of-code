9b4aaa4d34212649a07680ae71b502c4d46880f6
Kyuuhachi <caagr98@gmail.com>
2024-12-05 22:37:44 +0100

    244

--- 24/05.py
+++ 24/05.py
@@ -1,14 +1,11 @@
 S=str.split
 a,b=S(open("05.in").read(),"\n\n")
 p={(*S(a,"|"),)for a in S(a)}
-
 A=[0,0]
 for b in S(b):
  c=x,d=S(b,","),0
  while c:
   for a in range(c:=0,l:=len(x)-1):
-   if(x[a+1],x[a])in p:
-    x[a:a+2]=x[a+1],x[a]
-    c=d=1
+   if(x[a+1],x[a])in p:x[a:a+2]=x[a+1],x[a];c=d=1
  A[d]+=int(x[l//2])
 print(*A)
