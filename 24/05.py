273b4cee146dfb83cfb0177e8cf56806465115c4
Kyuuhachi <caagr98@gmail.com>
2024-12-05 22:05:41 +0100

    495→362

--- 24/05.py
+++ 24/05.py
@@ -4,20 +4,16 @@ for a in a.split():
  a1, a2 = a.split("|")
  pairs.add((a1, a2))
 
-A=B=0
+A=[0,0]
 for b in b.split():
  x = b.split(",")
- any_bad = any((x[b], x[a]) in pairs for a in range(len(x)) for b in range(a+1, len(x)))
- if any_bad:
-  c=1
-  while c:
-   c=0
-   for a in range(len(x)):
-    for b in range(a+1, len(x)):
-     if (x[a], x[a+1]) in pairs:
-      x[a], x[a+1] = x[a+1], x[a]
-      c=1
-  B += int(x[len(x)//2])
- else:
-  A += int(x[len(x)//2])
-print(A,B)
+ c=1;d=0
+ while c:
+  c=0
+  for a in range(len(x)):
+   for b in range(a+1, len(x)):
+    if (x[a+1], x[a]) in pairs:
+     x[a], x[a+1] = x[a+1], x[a]
+     c=d=1
+ A[d] += int(x[len(x)//2])
+print(*A)
