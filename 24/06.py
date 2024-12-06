dbb02cd13eb3d7a2426531b90d9af0258189d9fc
Kyuuhachi <caagr98@gmail.com>
2024-12-07 00:36:36 +0100

    389

--- 24/06.py
+++ 24/06.py
@@ -7,13 +7,13 @@ d=0
 m=set()
 K=set()
 def run(pos,d,v,P):
- if P in K:return
+ if{P}<K:return
  K.add(P)
  while 1:
   np=pos+stride[d%4]
   n=s[np:][:1]
   if P==s:run(pos,d,{*v},np)
-  if(pos,d%4)in v:m.add(P);break
+  if{(pos,d%4)}<v:m.add(P);break
   v.add((pos,d%4))
   if'#'>n or np<0:break
   if'#'==n or np==P:d+=1
