35deb8602c80446787139ee5a5e7515ed32cf740
Kyuuhachi <caagr98@gmail.com>
2024-12-07 00:30:34 +0100

    411→407

--- 24/06.py
+++ 24/06.py
@@ -14,12 +14,12 @@ def run(pos,d,v,P):
   np=pos+stride[d%4]
   n=s[np:][:1]
   if P==s:run(pos,d,{*v},np)
+  if(pos,d%4)in v:m.add(P);break
+  v.add((pos,d%4))
   if'#'>n or np<0:break
   if'#'==n or np==P:d+=1
   else:pos=np
-  if(pos,d%4)in v:m.add(P);break
-  v.add((pos,d%4))
 
-v={(pos,0)}
+v=set()
 run(pos,0,v,s)
 print(len({a for a,_ in v}),len(m))
