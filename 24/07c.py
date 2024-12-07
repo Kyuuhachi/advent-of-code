ede823a9364be8db455a86d178bdecd7731a83f9
Kyuuhachi <caagr98@gmail.com>
2024-12-07 20:01:31 +0100

    224

--- 24/07c.py
+++ 24/07c.py
@@ -1,4 +1,4 @@
 r=lambda a,c:W*(a==Q)if a%1 or[]==c else r(a-(b:=c[0]),c:=c[1:])|r(a/b,c)|O*r((a-b)/10**len(str(b)),c)
 A=B=0
-for line in open("07.in"):W,Q,*c,=map(int,line.replace(*': ').split());c=c[::-1];O=0;A+=r(W,c);O=1;B+=r(W,c)
+for line in open("07.in"):*c,Q,W=map(int,line.replace(*': ').split()[::-1]);O=0;A+=r(W,c);O=1;B+=r(W,c)
 print(A,B)
