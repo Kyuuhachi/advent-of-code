1454f1290032209c354694e5aae16981177ff802
Kyuuhachi <caagr98@gmail.com>
2024-12-07 12:15:35 +0100

    274

--- 24/07c.py
+++ 24/07c.py
@@ -1,16 +1,16 @@
 A = B = 0
-def r(a,*c):
- if()==c:return(a==Q)*W
+def r(a,c):
+ if[]==c:return(a==Q)*W
  b,*c=c
- v=r(a-b,*c)
- if a%b==0:v|=r(a//b,*c)
+ v=r(a-b,c)
+ if a%b==0:v|=r(a//b,c)
  n=10**len(str(b))
- if O&(a%n==b):v|=r(a//n,*c)
+ if O&(a%n==b):v|=r(a//n,c)
  return v
 
 for line in open("07.in"):
  W,Q,*c,=map(int,line.replace(*': ').split())
  c=c[::-1]
- O=0;A+=r(W,*c)
- O=1;B+=r(W,*c)
+ O=0;A+=r(W,c)
+ O=1;B+=r(W,c)
 print(A, B)
