be1501a277d62cfdc79d8a8b0769b940bfd22e8a
Kyuuhachi <caagr98@gmail.com>
2024-12-07 01:17:58 +0100

    311

--- 24/06.py
+++ 24/06.py
@@ -2,12 +2,12 @@ s=open("06.in").read()
 w=s.find('\n')+1
 m=set()
 K={()}
-def run(p,d,v,P):
+def r(p,d,v,P):
  while not{P}<K:
   N=p+[-w,1,w,-1][d];n=s[N:][:1]
-  if P==s:run(p,d,{*v},N)
+  if P==s:r(p,d,{*v},N)
   if{k:=(p,d)}<v:m.add(P);n=''
   v.add(k);p=[N,p][q:=n=='#'or N==P];d=d+q&3
   if'#'>n or N<0:K.add(P)
-run(s.find('^'),0,v:=set(),s)
+r(s.find('^'),0,v:=set(),s)
 print(len({a for a,_ in v}),len(m))
