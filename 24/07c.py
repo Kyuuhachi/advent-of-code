11e3f2f29dc83e252f890f0feda3902003f7921d
Kyuuhachi <caagr98@gmail.com>
2024-12-07 19:53:32 +0100

    244

--- 24/07c.py
+++ 24/07c.py
@@ -2,10 +2,7 @@ A = B = 0
 def r(a,c):
  if a%1 or not c:return(a==Q)*W
  b,*c=c
- v=r(a-b,c)
- v|=r(a/b,c)
- v|=O*r((a-b)/10**len(str(b)),c)
- return v
+ return r(a-b,c)|r(a/b,c)|O*r((a-b)/10**len(str(b)),c)
 
 for line in open("07.in"):
  W,Q,*c,=map(int,line.replace(*': ').split())
