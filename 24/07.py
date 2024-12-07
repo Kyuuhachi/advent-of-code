2f4275545f6c1737d8bf0dcac72f926fb6cfb6c7
Kyuuhachi <caagr98@gmail.com>
2024-12-07 09:35:40 +0100

    Lists are twice as fast

--- 24/07.py
+++ 24/07.py
@@ -1,8 +1,8 @@
 A=B=0
 i=int
-k=lambda c,p:{v*c for v in p}|{v+c for v in p}
+k=lambda c,p:[v*c for v in p]+[v+c for v in p]
 for l in open("07.in"):
- a,b,*c=l.split();p=q={i(b)}
- for c in c:p=k(i(c),p);q=k(i(c),q)|{i(str(v)+c)for v in q}
+ a,b,*C=l.split();p=q=[i(b)]
+ for c in C:p=k(i(c),p);q=k(i(c),q)+[i(str(v)+c)for v in q]
  a=i(a[:-1]);A+=a*(a in p);B+=a*(a in q)
 print(A,B)
