2fdac0d52a1cacf181be9731cc00c2b152a70715
Kyuuhachi <caagr98@gmail.com>
2024-12-07 01:45:12 +0100

    299

--- 24/06.py
+++ 24/06.py
@@ -1,11 +1,10 @@
-s=open("06.in").read()
 m=set()
-def r(p,d,v,P,K={()}):
+def r(P,p,d,v,K={()}):
  while{P}-K:
   N=p-[w:=s.find('\n')+1,-1,-w,1][d];n=s[N:N+1]
-  if P==s:r(p,d,{*v},N)
+  if P==s:r(N,p,d,{*v})
   if{k:=(p,d)}<v:m.add(P);n=s
   v|={k};p=[N,p][q:=n=='#'or N==P];d=d+q&3
   if'#'>n or N<0:K|={P}
-r(s.find('^'),0,v:=set(),s)
+r(s:=open("06.in").read(),s.find('^'),0,v:=set())
 print(len({a for a,_ in v}),len(m))
