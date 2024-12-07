5a834469579656c2f767113e640651e3eab5c81a
Kyuuhachi <caagr98@gmail.com>
2024-12-07 09:25:26 +0100

    245

--- 24/07.py
+++ 24/07.py
@@ -1,10 +1,11 @@
 A=B=0
+k=lambda c,p:{v*c for v in p}|{v+c for v in p}
 for l in open("07.in"):
  a,b,*c=map(int,l.replace(':','').split())
  p={b};q={b}
  for c in c:
-  p = {v*c for v in p}|{v+c for v in p}
-  q = {v*c for v in q}|{v+c for v in q}|{int(f"{v}{c}") for v in q}
+  p = k(c,p)
+  q = k(c,q)|{int(f"{v}{c}") for v in q}
  A += a*(a in p)
  B += a*(a in q)
 print(A,B)
