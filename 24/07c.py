fafeeaf9dbbf8847781bbb8bc57c684e06d3d435
Kyuuhachi <caagr98@gmail.com>
2024-12-07 20:04:36 +0100

    217

--- 24/07c.py
+++ 24/07c.py
@@ -1,4 +1,4 @@
-r=lambda a,c:W*(a==Q)if a%1 or[]==c else r(a-(b:=c[0]),c:=c[1:])|r(a/b,c)|O*r((a-b)/10**len(str(b)),c)
+r=lambda a,c:W*(a==Q)if a%1 or[]==c else O*r(10**-len(str(b:=c[0]))*(a-b),c:=c[1:])|r(a-b,c)|r(a/b,c)
 A=B=0
 for l in open("07.in"):*c,Q,W=map(int,l.replace(*': ').split()[::-1]);O=0;A+=r(W,c);O=1;B+=r(W,c)
 print(A,B)
