35ca11842a66db8f1b725edc17b4087ee575737a
Kyuuhachi <caagr98@gmail.com>
2024-12-07 01:37:24 +0100

    301

--- 24/06.py
+++ 24/06.py
@@ -2,7 +2,7 @@ s=open("06.in").read()
 w=s.find('\n')+1
 m=set()
 def r(p,d,v,P,K={()}):
- while not{P}<K:
+ while{P}-K:
   N=p+[-w,1,w,-1][d];n=s[N:N+1]
   if P==s:r(p,d,{*v},N)
   if{k:=(p,d)}<v:m.add(P);n=s
