390f60300244b9ab7aef5cc2a3a6ea0c2e7ed4f0
Kyuuhachi <caagr98@gmail.com>
2024-12-07 10:45:03 +0100

    202

--- 24/07b.py
+++ 24/07b.py
@@ -1,4 +1,4 @@
-F=lambda b,c:(lambda b,c,*d:F(b+c,d)or F(b*c,d)or O*F(int(f"{b}{c}"),d))(b,*c)if c else b==a
+F=lambda b,c:(lambda c,*d:F(b+c,d)or F(b*c,d)or O*F(int(f"{b}{c}"),d))(*c)if c else b==a
 A=B=0
 for l in open("07.in"):a,b,*c=map(int,l.replace(*': ').split());O=0;A+=a*F(b,c);O=1;B+=a*F(b,c)
 print(A,B)
