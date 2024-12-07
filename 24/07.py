7d709be305042d21e129fc00911f3a07eae64510
Kyuuhachi <caagr98@gmail.com>
2024-12-07 09:32:21 +0100

    224

--- 24/07.py
+++ 24/07.py
@@ -3,6 +3,6 @@ i=int
 k=lambda c,p:{v*c for v in p}|{v+c for v in p}
 for l in open("07.in"):
  a,b,*c=l.split();p=q={i(b)}
- for c in c:p=k(i(c),p);q=k(i(c),q)|{i(f"{v}{c}")for v in q}
+ for c in c:p=k(i(c),p);q=k(i(c),q)|{i(str(v)+c)for v in q}
  a=i(a[:-1]);A+=a*(a in p);B+=a*(a in q)
 print(A,B)
