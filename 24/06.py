69c819dc097b8b5d8c1b310f42979a95628031a2
Kyuuhachi <caagr98@gmail.com>
2024-12-07 01:56:56 +0100

    297

--- 24/06.py
+++ 24/06.py
@@ -2,9 +2,10 @@ m=set()
 def r(P,p,d,v,K={()}):
  while{P}-K:
   N=p-[w:=s.find('\n')+1,-1,-w,1][d];n=s[N:N+1]
-  if P==s:r(N,p,d,{*v})
+  P or r(N,p,d,{*v})
   if{k:=(p,d)}<v:m.add(P);n=s
   v|={k};p=[N,p][q:=n=='#'or N==P];d=d+q&3
   if'#'>n or N<0:K|={P}
-r(s:=open("06.in").read(),s.find('^'),0,v:=set())
+s=open("06.in").read()
+r(0,s.find('^'),0,v:=set())
 print(len({a for a,_ in v}),len(m))
