bcb94c807e71decb79d78982d195c216c9621383
Kyuuhachi <caagr98@gmail.com>
2024-12-07 01:34:46 +0100

    305, not sure why this works

--- 24/06.py
+++ 24/06.py
@@ -5,7 +5,7 @@ def r(p,d,v,P,K={()}):
  while not{P}<K:
   N=p+[-w,1,w,-1][d];n=s[N:N+1]
   if P==s:r(p,d,{*v},N)
-  if{k:=(p,d)}<v:m.add(P);n=''
+  if{k:=(p,d)}<v:m.add(P);n=s
   v|={k};p=[N,p][q:=n=='#'or N==P];d=d+q&3
   if'#'>n or N<0:K|={P}
 r(s.find('^'),0,v:=set(),s)
