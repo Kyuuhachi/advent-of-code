7fc476959d52335c67e35404aeb58ecd28ef911e
Kyuuhachi <caagr98@gmail.com>
2024-12-07 12:12:46 +0100

    282

--- 24/07c.py
+++ 24/07c.py
@@ -1,17 +1,16 @@
 A = B = 0
-def solve(a, *c):
+def r(a,*c):
  if()==c:return(a==Q)*W
  b,*c=c
- v = 0
- if not a % b: v |= solve(a//b, *c)
- if a > b: v |= solve(a-b, *c)
- n = 10**len(str(b))
- if O and a % n == b: v |= solve(a//n, *c)
+ v=r(a-b,*c)
+ if a%b==0:v|=r(a//b,*c)
+ n=10**len(str(b))
+ if O and a%n==b:v|=r(a//n,*c)
  return v
 
 for line in open("07.in"):
  W,Q,*c,=map(int,line.replace(*': ').split())
  c=c[::-1]
- O=0;A+=solve(W,*c)
- O=1;B+=solve(W,*c)
+ O=0;A+=r(W,*c)
+ O=1;B+=r(W,*c)
 print(A, B)
