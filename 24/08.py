ee657e5a5c7391fab625edab6c3f70716d9102e6
Kyuuhachi <caagr98@gmail.com>
2024-12-09 00:24:03 +0100

    298

--- 24/08.py
+++ 24/08.py
@@ -5,5 +5,5 @@ i=0
 for c in s:m[c]|={i//w+i%w*1j};i+=1
 m[10]=m[46]=[]
 R=range(w-1)
-f=lambda f,g:len({v for n in m for p in n for q in n for d in g if f(n,v:=p+(p-q)*d)}&{i+j*1j for i in R for j in R})
-print(f(lambda n,v:{v}-n,[1]),f(lambda*_:1,R))
+f=lambda f,g:len({v for n in m for p in n for q in n for d in g if f(v:=p+(p-q)*d,n)}&{i+j*1j for i in R for j in R})
+print(f(lambda v,n:{v}-n,[1]),f(map,R))
