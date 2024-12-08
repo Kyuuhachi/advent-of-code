3e520fbf7416e2eab5b266f81b558d349a836784
Kyuuhachi <caagr98@gmail.com>
2024-12-08 21:44:20 +0100

    307

--- 24/08.py
+++ 24/08.py
@@ -1,7 +1,8 @@
 s=open("08.in",'rb').read()
 m=[set()for _ in range(256)]
 w=s.find(10)+1
-for i,c in enumerate(s):m[c]|={i//w+i%w*1j}
+i=0
+for c in s:m[c]|={i//w+i%w*1j};i+=1
 m[10]=m[46]=[]
 R=range(w-1)
 f=lambda f,g:len({v for n in m for p in n for q in n for d in g if f(n,v:=p+(p-q)*d)}&{i+j*1j for i in R for j in R})
