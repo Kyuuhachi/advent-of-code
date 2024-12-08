9edc62d73753919b1da88940b820ada0351a7bb6
Kyuuhachi <caagr98@gmail.com>
2024-12-08 21:43:54 +0100

    311

--- 24/08.py
+++ 24/08.py
@@ -4,12 +4,5 @@ w=s.find(10)+1
 for i,c in enumerate(s):m[c]|={i//w+i%w*1j}
 m[10]=m[46]=[]
 R=range(w-1)
-f=lambda f,g:len({
- v
- for n in m
- for p in n
- for q in n
- for d in g
- if f(n,v:=p+(p-q)*d)
-}&{i+j*1j for i in R for j in R})
+f=lambda f,g:len({v for n in m for p in n for q in n for d in g if f(n,v:=p+(p-q)*d)}&{i+j*1j for i in R for j in R})
 print(f(lambda n,v:{v}-n,[1]),f(lambda n,v:1,R))
