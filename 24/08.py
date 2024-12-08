c134bb85cd1a8dcb66dbcc475f62aacf4be2dbd2
Kyuuhachi <caagr98@gmail.com>
2024-12-09 00:45:59 +0100

    281

--- 24/08.py
+++ 24/08.py
@@ -3,7 +3,9 @@ m=[set()for _ in s]
 w=s.find(10)+1
 i=0
 for c in s:m[c]|={i//w+i%w*1j};i+=1
-m[10]=m[46]=[]
+m[10]=[]
+A=set().union(*m)
+m[46]=[]
 R=range(w-1)
-f=lambda f,g:len({v for n in m for p in n for q in n for d in g if f(v:=p+(p-q)*d,n)}&{i+j*1j for i in R for j in R})
+f=lambda f,g:len({v for n in m for p in n for q in n for d in g if f(v:=p+(p-q)*d,n)}&A)
 print(f(lambda v,n:{v}-n,[1]),f(map,R))
