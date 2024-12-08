f78fc313878d46369c17212044c74dbd65be2b6f
Kyuuhachi <caagr98@gmail.com>
2024-12-08 19:54:13 +0100

    Change bounds check

--- 24/08.py
+++ 24/08.py
@@ -3,14 +3,14 @@ m=[[]for _ in range(256)]
 w=s.find(10)+1
 for i,c in enumerate(s):m[c]+=[(i//w,i%w)]
 m[10]=m[46]=[]
+a={(i,j)for i in range(w-1)for j in range(w-1)}
 A={
  (x,y)
  for n in m
  for p,q in n
  for r,s in n
  if (x:=p+p-r,y:=q+q-s)not in n
- if 0<x+1<w if 0<y+1<w
-}
+}&a
 B={
  (x,y)
  for n in m
@@ -18,12 +18,5 @@ B={
  for r,s in n
  for d in range(w)
  if (x:=p+(r-p)*d,y:=q+(s-q)*d)
- if 0<x+1<w if 0<y+1<w
-}
-
+}&a
 print(len(A),len(B))
-
-for i,c in enumerate(s):
- k=i//w,i%w
- if k in B:print(f"\x1B\x5B7m{c:c}\x1B\x5B0m",end="")
- else:print(chr(c),end="")
