d070ef71d268834a5b77605cc3d33f6f70279451
Kyuuhachi <caagr98@gmail.com>
2024-12-09 01:25:51 +0100

    269

--- 24/08.py
+++ 24/08.py
@@ -1,10 +1,10 @@
 s=open("08.in",'rb').read()
 m=[set()for _ in s]
+A={0}
 i=j=0
 for c in s:
- if c-10:m[c]|={i+j};i+=1
+ if c-10:m[c]|={i+j};i+=1;A|={i+j}
  else:j+=1j;i=0
-A={0}.union(*m)
 m[46]=[]
 f=lambda f,g:len({v for n in m for p in n for q in n for d in g if f(v:=p+(p-q)*d,n)}&A)
 print(f(lambda v,n:{v}-n,[1]),f(map,range(99)))
