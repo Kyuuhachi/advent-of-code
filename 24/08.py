31f35e416b0b678af867c79d4b88c13a4f3cf510
Kyuuhachi <caagr98@gmail.com>
2024-12-09 01:53:33 +0100

    241, thanks trillian

--- 24/08.py
+++ 24/08.py
@@ -6,5 +6,5 @@ for c in s:
  if c-10:m[c]|={i};i+=1j;A|=m[c]
  else:i=i.real+1
 m[46]=[]
-f=lambda f,g:len({p+v for n in m for p in n for q in n for d in range(g)if(v:=(p-q)*d)+f}&A)
-print(f(0,2),f(i,99))
+f=lambda f,g:len({p+d*(p-q)for n in m for p in n for q in n for d in range(f,g)if p-q}&A)
+print(f(1,2),f(0,99))
