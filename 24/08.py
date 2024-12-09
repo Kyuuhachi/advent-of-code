46677c5bdfabcd82e65f5442bf2481b7e903c3c6
Kyuuhachi <caagr98@gmail.com>
2024-12-09 01:51:53 +0100

    244

--- 24/08.py
+++ 24/08.py
@@ -6,5 +6,5 @@ for c in s:
  if c-10:m[c]|={i};i+=1j;A|=m[c]
  else:i=i.real+1
 m[46]=[]
-f=lambda f,g:len({p+v for n in m for p in n for q in n for d in range(g)if(v:=(p-q)*d)or f}&A)
-print(f(0,2),f(1,99))
+f=lambda f,g:len({p+v for n in m for p in n for q in n for d in range(g)if(v:=(p-q)*d)+f}&A)
+print(f(0,2),f(i,99))
