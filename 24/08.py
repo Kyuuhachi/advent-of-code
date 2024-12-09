6fe7625f81db5033fcd40c23935f21a41a11a938
Kyuuhachi <caagr98@gmail.com>
2024-12-09 01:32:05 +0100

    Revert "264, and undid the previous slowdown"
    This reverts commit 7381628c3917835ef5155fcaa52833ed18aadc60.

--- 24/08.py
+++ 24/08.py
@@ -3,7 +3,7 @@ m=[set()for _ in s]
 A={0}
 i=j=0
 for c in s:
- if c-10:m[c]|=t={i+j};i+=1;A|=t
+ if c-10:m[c]|={i+j};i+=1;A|=m[c]
  else:j+=1j;i=0
 m[46]=[]
 f=lambda f,g:len({v for n in m for p in n for q in n for d in range(g)if f(v:=p+(p-q)*d,n)}&A)
