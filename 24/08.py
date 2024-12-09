0a6614e5321b3e7d4ee18c360b7b73cac98e8b88
Kyuuhachi <caagr98@gmail.com>
2024-12-09 01:36:08 +0100

    263, thanks trillian

--- 24/08.py
+++ 24/08.py
@@ -1,10 +1,10 @@
 s=open("08.in",'rb').read()
 m=[set()for _ in s]
 A={0}
-i=j=0
+i=0
 for c in s:
- if c-10:m[c]|={i+j};i+=1;A|=m[c]
- else:j+=1j;i=0
+ if c-10:m[c]|={i};i+=1j;A|=m[c]
+ else:i=i.real+1
 m[46]=[]
 f=lambda f,g:len({v for n in m for p in n for q in n for d in range(g)if f(v:=p+(p-q)*d,n)}&A)
 print(f(lambda v,n:{v}-n,2),f(map,99))
