b1849146ec6c01eebd68c30d029dcebe972cd511
Kyuuhachi <caagr98@gmail.com>
2024-12-10 18:39:42 +0100

    353

--- 24/09.py
+++ 24/09.py
@@ -1,12 +1,9 @@
-*s,=map(int,open("09.in").read().strip())
+*s,=map(int,open("09.in").read().strip()+'0')
 for z in 0,1:
- n=0
+ n=i=0
  R=[]
  H=[]
- for i,w in enumerate(s):
-  if~i%2:R+=[[n,w,i//2]]if z else[[n+m,1,i//2]for m in range(w)]
-  else:H+=[[n,w]]
-  n+=w
+ for w,v in zip(a:=iter(s),a):R+=[[n,w,i]]if z else[[n+m,1,i]for m in range(w)];H+=[[n+w,v]];n+=v+w;i+=1
  for r in R[::-1]:
   for h in H:
    if h[1]>=r[1]:r[0]=h[0];h[0]+=r[1];h[1]-=r[1];h[1]or H.remove(h)
