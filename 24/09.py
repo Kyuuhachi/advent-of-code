6365833497e1f20857b13a429ba57427368552d2
Kyuuhachi <caagr98@gmail.com>
2024-12-10 19:33:27 +0100

    Fix wrong result

--- 24/09.py
+++ 24/09.py
@@ -3,7 +3,7 @@ for z in 0,1:
  for w,v in zip(a:=map(int,open("09.in").read().strip()+'0'),a):R+=[[n,w,i]]if z else[[n+m,1,i]for m in range(w)];H+=[[n+w,v]];n+=v+w;i+=1
  for n,w,i in R[::-1]:
   for h in H:
-   if h[1]>=w:n=h[0];h[0]+=w;h[1]-=w;h[1]or H.remove(h)
    if h[0]>=n:break
+   if h[1]>=w:n=h[0];h[0]+=w;h[1]-=w;h[1]or H.remove(h)
   S+=i*w*(2*n+w-1)//2
  print(S)
