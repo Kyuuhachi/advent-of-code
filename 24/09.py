139be38702a1de852201e47983498ae7b8c69bab
Kyuuhachi <caagr98@gmail.com>
2024-12-10 18:26:07 +0100

    416→364

--- 24/09.py
+++ 24/09.py
@@ -1,22 +1,16 @@
 *s,=map(int,open("09.in").read().strip())
-E=enumerate
-
-a=[]
-for i,w in E(s[::2]):a+=[i]*w
-o=[]
-for w in s:o+=a[:w];a=a[w:][::-1]
-print(sum(i*v for i,v in E(o)))
-
-n=0
-R=[]
-H=[]
-for i,w in E(s):
- if~i%2:R+=[[n,w,i//2]]
- else:H+=[[n,w]]
- n+=w
-for r in R[::-1]:
- print(r)
- for h in H:
-  if h[1]>=r[1]:r[0]=h[0];h[0]+=r[1];h[1]-=r[1];h[1]or H.remove(h)
-  if h[0]>=r[0]:break
-print(sum(i*k*a+i*k*(k-1)//2 for a,k,i in R))
+def Z(z):
+ n=0
+ R=[]
+ H=[]
+ for i,w in enumerate(s):
+  if~i%2:R+=[[n,w,i//2]]if z else[[n+m,1,i//2]for m in range(w)]
+  else:H+=[[n,w]]
+  n+=w
+ for r in R[::-1]:
+  for h in H:
+   if h[1]>=r[1]:r[0]=h[0];h[0]+=r[1];h[1]-=r[1];h[1]or H.remove(h)
+   if h[0]>=r[0]:break
+ print(sum(i*k*(2*a+k-1)//2 for a,k,i in R))
+Z(0)
+Z(1)
