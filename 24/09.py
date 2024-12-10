791222af44ada5b6f5b2e0f6c3fcbc0734f2079a
Kyuuhachi <caagr98@gmail.com>
2024-12-10 18:49:03 +0100

    334

--- 24/09.py
+++ 24/09.py
@@ -4,8 +4,10 @@ for z in 0,1:
  R=[]
  H=[]
  for w,v in zip(a:=iter(s),a):R+=[[n,w,i]]if z else[[n+m,1,i]for m in range(w)];H+=[[n+w,v]];n+=v+w;i+=1
- for r in R[::-1]:
+ S=0
+ for a,k,i in R[::-1]:
   for h in H:
-   if h[1]>=r[1]:r[0]=h[0];h[0]+=r[1];h[1]-=r[1];h[1]or H.remove(h)
-   if h[0]>=r[0]:break
- print(sum(i*k*(2*a+k-1)//2 for a,k,i in R))
+   if h[1]>=k:a=h[0];h[0]+=k;h[1]-=k;h[1]or H.remove(h)
+   if h[0]>=a:break
+  S+=i*k*(2*a+k-1)//2
+ print(S)
