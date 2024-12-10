0a9fe45ed28e085eb846ea7191cbffaa63588bdf
Kyuuhachi <caagr98@gmail.com>
2024-12-10 18:06:24 +0100

    Bigger but faster

--- 24/09.py
+++ 24/09.py
@@ -8,15 +8,15 @@ for w in s:o+=a[:w];a=a[w:][::-1]
 print(sum(i*v for i,v in E(o)))
 
 n=0
-r=[]
+R=[]
+H=[]
 for i,w in E(s):
- if~i%2:r+=[[n,w,i//2]]
+ if~i%2:R+=[[n,w,i//2]]
+ else:H+=[[n,w]]
  n+=w
-for k in r[::-1]:
- print(k)
- for(a,b,c),(d,e,f)in zip(r,r[1:]):
-  if d-a-b>=k[1] and a<k[0]:
-   k[0]=a+b
-   r.sort()
-   break
-print(sum(i*k*a+i*k*(k-1)//2 for a,k,i in r))
+for r in R[::-1]:
+ print(r)
+ for h in H:
+  if h[1]>=r[1]:r[0]=h[0];h[0]+=r[1];h[1]-=r[1]
+  if h[0]>=r[0]:break
+print(sum(i*k*a+i*k*(k-1)//2 for a,k,i in R))
