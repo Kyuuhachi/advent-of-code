b8b2ddd04330552878253e0e9ffaab6f0b452f79
Kyuuhachi <caagr98@gmail.com>
2024-12-10 20:27:16 +0100

    248

--- 24/10.py
+++ 24/10.py
@@ -1,11 +1,11 @@
 s=open("10.in",'rb').read()
 w=s.find(10)+1
-def R(p,c):
+def R(p,c=48):
  if 0<=p<len(s)and s[p]==c:return{F(p)}if c==57 else R(p-1,c:=c+1)|R(p+1,c)|R(p+w,c)|R(p-w,c)
  return set()
 A=B=i=0
 for c in s:
- F=abs;A+=len(R(i,48))
- F=property;B+=len(R(i,48))
+ F=abs;A+=len(R(i))
+ F=property;B+=len(R(i))
  i+=1
 print(A,B)
