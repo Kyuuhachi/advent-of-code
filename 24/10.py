3379d19c5553e3a417fdf9c72fbbb84ba6d8b28e
Kyuuhachi <caagr98@gmail.com>
2024-12-10 20:30:05 +0100

    240

--- 24/10.py
+++ 24/10.py
@@ -1,8 +1,6 @@
 s=open("10.in",'rb').read()
 w=s.find(10)+1
-def R(p,c=48):
- if 0<=p<len(s)and s[p]==c:return{F(p)}if c==57 else R(p-1,c:=c+1)|R(p+1,c)|R(p+w,c)|R(p-w,c)
- return set()
+R=lambda p,c=48:({F(p)}if c==57 else R(p-1,c:=c+1)|R(p+1,c)|R(p+w,c)|R(p-w,c))if 0<=p<len(s)and s[p]==c else set()
 A=B=i=0
 for c in s:
  F=abs;A+=len(R(i))
