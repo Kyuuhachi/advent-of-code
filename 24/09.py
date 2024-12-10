215f7469b23c603ee27287055db8d21b39eb2ca3
Kyuuhachi <caagr98@gmail.com>
2024-12-10 19:08:29 +0100

    Use consistent variable names

--- 24/09.py
+++ 24/09.py
@@ -1,9 +1,9 @@
 for z in 0,1:
  S=n=i=0;R,H=[],[]
  for w,v in zip(a:=map(int,open("09.in").read().strip()+'0'),a):R+=[[n,w,i]]if z else[[n+m,1,i]for m in range(w)];H+=[[n+w,v]];n+=v+w;i+=1
- for a,k,i in R[::-1]:
+ for n,w,i in R[::-1]:
   for h in H:
-   if h[1]>=k:a=h[0];h[0]+=k;h[1]-=k;h[1]or H.remove(h)
-   if h[0]>=a:break
-  S+=i*k*(2*a+k-1)//2
+   if h[1]>=w:n=h[0];h[0]+=w;h[1]-=w;h[1]or H.remove(h)
+   if h[0]>=n:break
+  S+=i*w*(2*n+w-1)//2
  print(S)
