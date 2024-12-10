062980b5f3e457624d0d04e601f34e955e6b72ea
Kyuuhachi <caagr98@gmail.com>
2024-12-10 20:15:00 +0100

    390→361

--- 24/10.py
+++ 24/10.py
@@ -5,19 +5,15 @@ def R(p,c):
   if c==57:return{p}
   return R(p-1,c+1)|R(p+1,c+1)|R(p+w,c+1)|R(p-w,c+1)
  return set()
-t=i=0
-for c in s:
- t+=len(R(i,48))
- i+=1
-print(t)
 
-def R(p,c):
+def G(p,c):
  if 0<=p<len(s)and s[p]==c:
   if c==57:return 1
-  return R(p-1,c+1)+R(p+1,c+1)+R(p+w,c+1)+R(p-w,c+1)
+  return G(p-1,c+1)+G(p+1,c+1)+G(p+w,c+1)+G(p-w,c+1)
  return 0
-t=i=0
+A=B=i=0
 for c in s:
- t+=R(i,48)
+ A+=len(R(i,48))
+ B+=G(i,48)
  i+=1
-print(t)
+print(A,B)
