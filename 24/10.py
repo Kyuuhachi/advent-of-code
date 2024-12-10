60acf4fc29cf3a9372d2cbca0a187b6965c3d05b
Kyuuhachi <caagr98@gmail.com>
2024-12-10 20:26:54 +0100

    251

--- 24/10.py
+++ 24/10.py
@@ -1,9 +1,7 @@
 s=open("10.in",'rb').read()
 w=s.find(10)+1
 def R(p,c):
- if 0<=p<len(s)and s[p]==c:
-  if c==57:return{F(p)}
-  return R(p-1,c:=c+1)|R(p+1,c)|R(p+w,c)|R(p-w,c)
+ if 0<=p<len(s)and s[p]==c:return{F(p)}if c==57 else R(p-1,c:=c+1)|R(p+1,c)|R(p+w,c)|R(p-w,c)
  return set()
 A=B=i=0
 for c in s:
