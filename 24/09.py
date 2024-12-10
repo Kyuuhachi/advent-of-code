12f8ba11c9aec8dc695cd53cc0af0d66b57664f9
Kyuuhachi <caagr98@gmail.com>
2024-12-10 19:37:05 +0100

    314

--- 24/09.py
+++ 24/09.py
@@ -1,6 +1,6 @@
 for z in 0,1:
  S=n=i=0;R,H=[],[]
- for w,v in zip(a:=map(int,open("09.in").read().strip()+'0'),a):R+=[[n,w,i]]if z else[[n+m,1,i]for m in range(w)];H+=[[n+w,v]];n+=v+w;i+=1
+ for w,v in zip(a:=map(int,open("09.in").read().strip()+'0'),a):R+=[[[n+m,1,i]for m in range(w)],[[n,w,i]]][z];H+=[[n+w,v]];n+=v+w;i+=1
  for n,w,i in R[::-1]:
   for h in H:
    if h[0]>=n:break
