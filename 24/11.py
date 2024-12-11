25452e5c3cf26d602088c217726877cdbe128e46
Kyuuhachi <caagr98@gmail.com>
2024-12-11 18:05:24 +0100

    288→272

--- 24/11.py
+++ 24/11.py
@@ -6,5 +6,4 @@ def F(i,j):
  if i == 0:return F(1,j-1)
  if(p:=len(s:=str(i)))%2:return F(i*2024,j-1)
  return F(int(s[:p//2]),j-1)+F(int(s[p//2:]),j-1)
-print(sum(F(v,25)for v in s))
-print(sum(F(v,75)for v in s))
+for i in 25,75:print(sum(F(v,i)for v in s))
