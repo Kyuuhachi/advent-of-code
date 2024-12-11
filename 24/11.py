452419ed2f1b0672806b9b154184f0a8dfaa7986
Kyuuhachi <caagr98@gmail.com>
2024-12-11 18:06:44 +0100

    250

--- 24/11.py
+++ 24/11.py
@@ -1,9 +1,4 @@
 *s,=map(int,open("11.in").read().split())
 import functools as f
-@f.cache
-def F(i,j):
- if j == 0:return 1
- if i == 0:return F(1,j-1)
- if(p:=len(s:=str(i)))%2:return F(i*2024,j-1)
- return F(int(s[:p//2]),j-1)+F(int(s[p//2:]),j-1)
+F=f.cache(lambda i,j:1 if j==0 else F(1,j-1)if i==0 else F(i*2024,j-1)if(p:=len(s:=str(i)))%2 else F(int(s[:p//2]),j-1)+F(int(s[p//2:]),j-1))
 for i in 25,75:print(sum(F(v,i)for v in s))
