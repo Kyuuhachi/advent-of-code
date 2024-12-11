a7a3b72dfce60971e5e231406b90686859845971
Kyuuhachi <caagr98@gmail.com>
2024-12-11 18:35:59 +0100

    234

--- 24/11.py
+++ 24/11.py
@@ -1,3 +1,3 @@
 import functools as f
-F=f.cache(lambda i,j:((F(i*2024,j-1)if(p:=len(s:=str(i)))%2 else F(int(s[:p//2]),j-1)+F(int(s[p//2:]),j-1))if i else F(1,j-1))if j else 1)
-for i in 25,75:print(sum(F(int(v),i)for v in open("11.in").read().split()))
+F=f.cache(lambda i,j:((F(i*2024,j)if(p:=len(s:=str(i)))%2 else F(int(s[:p//2]),j)+F(int(s[p//2:]),j))if i else F(1,j))if(j:=j-1)else 1)
+for i in 26,76:print(sum(F(int(v),i)for v in open("11.in").read().split()))
