28c2228a01b8f1c15d446e67de1be53580898aa4
Kyuuhachi <caagr98@gmail.com>
2024-12-12 21:21:46 +0100

    347

--- 24/12.py
+++ 24/12.py
@@ -1,12 +1,10 @@
 s=open("12.in","rb").read()
 w=s.find(10)+1
 s+=b'\n'*w
-
 class J:v=b=0
 def R(v):
  while v.v:v,v.v=v.v,v.v.v
  return v
-
 g=[J()for _ in s]
 for a in range(w*w):
  b,c,d=a-1,a+~w,a-w
@@ -14,6 +12,5 @@ for a in range(w*w):
   c,b,a,d=b,a,d,c;A=R(g[a]);B=R(g[b]);S=s[a]
   if S!=s[b]:A.b+=((S!=s[d])|(S==s[c]))+1j
   elif A!=B:A.b+=B.b;B.v=A
-
 R(g[a]).b=0
 print(sum(R(a).b for a in g))
