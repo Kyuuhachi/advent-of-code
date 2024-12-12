135694374a2a2d80d8d2e19df80d9d04f7ee71fb
Kyuuhachi <caagr98@gmail.com>
2024-12-12 21:17:47 +0100

    359

--- 24/12.py
+++ 24/12.py
@@ -9,7 +9,7 @@ def R(n):
 
 g=[J()for _ in s]
 for a in range(w*w):
- b,c,d=a-1,a-w-1,a-w
+ b,c,d=a-1,a+~w,a-w
  for()in[()]*4:
   c,b,a,d=b,a,d,c;A=R(g[a]);B=R(g[b]);S=s[a]
   if S!=s[b]:A.b+=((S!=s[d])|(S==s[c]))+1j
