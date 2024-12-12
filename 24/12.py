29c463764ca1573b354dfbffe950a9559d3ba581
Kyuuhachi <caagr98@gmail.com>
2024-12-12 22:21:05 +0100

    Split S to a separate assignment

--- 24/12.py
+++ 24/12.py
@@ -8,7 +8,7 @@ def R(v):
 g=[J()for _ in s]
 for a in range(w*w):
  for k in[a,b:=a-1,c:=a+~w,d:=a-w]:
-  d,A,B,c,S=a,R(g[a:=b]),R(g[b:=c]),d,s[a]
+  d,A,B,c=a,R(g[a:=b]),R(g[b:=c]),d;S=s[a]
   if S!=s[b]:A.b+=1-(s[d]==S!=s[c])+1j
   elif A!=B:A.b+=B.b;B.v=A
 R(g[a]).b=0
