c793121b4a41c194129214a4ed53daccb259051a
Kyuuhachi <caagr98@gmail.com>
2024-12-12 23:35:10 +0100

    332

--- 24/12.py
+++ 24/12.py
@@ -8,8 +8,8 @@ def R(v):
 g=[J()for _ in s]
 for a in range(w*w):
  for d in[a-w,a,b:=a-1,c:=b-w]:
-  d,A,B,c=a,R(g[a:=b]),R(g[b:=c]),d;S=s[a]
-  if S!=s[b]:A.b+=1-(s[d]==S!=s[c])+1j
+  d,A,B,c=a,R(g[a:=b]),R(g[b:=c]),d
+  if s[a]!=s[b]:A.b+=1-(s[d]==s[a]!=s[c])+1j
   elif A!=B:A.b+=B.b;B.v=A
 R(g[a]).b=0
 print(sum(R(a).b for a in g))
