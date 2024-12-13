eb171fe91a04ef0c29ca4c228108122b946318d0
Kyuuhachi <caagr98@gmail.com>
2024-12-14 00:23:03 +0100

    225

--- 24/13.py
+++ 24/13.py
@@ -5,10 +5,9 @@ for s in re.finditer(r'\D*(\d+)'*6,open("13.in").read()):
  a,b,c,d,x,y=map(int,s.groups())
 
  D=a*d-b*c
- a,b,c,d=d,-b,-c,a
  for i in 0,1:
-  A=a*x+c*y
-  B=b*x+d*y
+  A=d*x-c*y
+  B=a*y-b*x
   if A%D+B%D==0:
    k[i]+=(3*A+B)/D
   x+=W;y+=W
