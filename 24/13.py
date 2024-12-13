02d0ea750a257e7ac2c5c3fed5bd9460e89f15b0
Kyuuhachi <caagr98@gmail.com>
2024-12-14 00:20:16 +0100

    297→255

--- 24/13.py
+++ 24/13.py
@@ -1,20 +1,15 @@
 import re
-m=n=0
+k=[0,0]
+W=10000000000000
 for s in re.finditer(r'\D*(\d+)'*6,open("13.in").read()):
  a,b,c,d,x,y=map(int,s.groups())
 
  D=a*d-b*c
  a,b,c,d=d,-b,-c,a
-
- A=a*x+c*y
- B=b*x+d*y
- if A%D+B%D==0:
-  m+=(3*A+B)//D
-
- x+=10000000000000
- y+=10000000000000
- A=a*x+c*y
- B=b*x+d*y
- if A%D+B%D==0:
-  n+=(3*A+B)//D
-print(m,n)
+ for i in 0,1:
+  A=a*x+c*y
+  B=b*x+d*y
+  if A%D+B%D==0:
+   k[i]+=(3*A+B)//D
+  x+=W;y+=W
+print(*k)
