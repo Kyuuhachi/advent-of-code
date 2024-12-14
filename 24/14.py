06fe46c512055feba3c710325f9c270166256ad2
Kyuuhachi <caagr98@gmail.com>
2024-12-14 11:00:02 +0100

    437

--- 24/14.py
+++ 24/14.py
@@ -7,9 +7,8 @@ for l in open("14.in"):
 K=lambda X,Y:sum(((y+A*100)%H-H//2)*Y>0<X*((x+B*100)%W-W//2)for x,y,B,A in R)
 print(K(1,1)*K(1,-1)*K(-1,1)*K(-1,-1))
 
-C=lambda a,b:(a*5151+b*5253)%10403
 import zlib
 g=lambda a:len(zlib.compress(bytes(i in a for i in range(H))))
 A=max(range(W),key=lambda t:g({(x+X*t)%W for x,y,X,Y in R}))
 B=max(range(H),key=lambda t:g({(y+Y*t)%H for x,y,X,Y in R}))
-print(C(B,A))
+print((B*5151+A*5253)%10403)
