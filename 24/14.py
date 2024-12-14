23e75fed7282218cd06668452e4042897aa39d81
Kyuuhachi <caagr98@gmail.com>
2024-12-14 11:07:09 +0100

    418

--- 24/14.py
+++ 24/14.py
@@ -1,8 +1,6 @@
 import re
-R=[]
 W,H=101,103
-for l in open("14.in"):
- R+=[[*map(int,re.findall(r"-?\d+",l))]]
+R=[[*map(int,re.findall(r"-?\d+",l))]for l in open("14.in")]
 
 K=lambda a,b:sum(((y+Y*100)%H-H//2)*b>0<a*((x+X*100)%W-W//2)for x,y,X,Y in R)
 print(K(1,1)*K(1,-1)*K(-1,1)*K(-1,-1))
