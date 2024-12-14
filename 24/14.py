b64e5f199a82105255f64e4976b7ebcefa3fae89
Kyuuhachi <caagr98@gmail.com>
2024-12-14 11:08:04 +0100

    412

--- 24/14.py
+++ 24/14.py
@@ -1,13 +1,12 @@
 import re
-W,H=101,103
 R=[[*map(int,re.findall(r"-?\d+",l))]for l in open("14.in")]
 
-K=lambda a,b:sum(((y+Y*100)%H-51)*b>0<a*((x+X*100)%W-50)for x,y,X,Y in R)
+K=lambda a,b:sum(((y+Y*100)%103-51)*b>0<a*((x+X*100)%101-50)for x,y,X,Y in R)
 print(K(1,1)*K(1,-1)*K(-1,1)*K(-1,-1))
 
 import zlib
-r=range(H)
+r=range(103)
 g=lambda a:len(zlib.compress(bytes(i in a for i in r)))
-A=max(r,key=lambda t:g({(x+X*t)%W for x,y,X,Y in R}))
-B=max(r,key=lambda t:g({(y+Y*t)%H for x,y,X,Y in R}))
+A=max(r,key=lambda t:g({(x+X*t)%101 for x,y,X,Y in R}))
+B=max(r,key=lambda t:g({(y+Y*t)%103 for x,y,X,Y in R}))
 print((B*5151+A*5253)%10403)
