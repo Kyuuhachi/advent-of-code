a9a9d4c24404e4842ced0cfa24c89122de356111
Kyuuhachi <caagr98@gmail.com>
2024-12-14 11:22:37 +0100

    313

--- 24/14.py
+++ 24/14.py
@@ -2,6 +2,7 @@ import re
 R=[[*map(int,re.findall(r"-?\d+",l))]for l in open("14.in")]
 K=lambda a,b:sum(((y+Y*100)%103-51)*b>0<a*((x+X*100)%101-50)for x,y,X,Y in R)
 r=range(99)
-A=min(r,key=lambda t:len({(r[0]+r[2]*t)%101 for r in R}))
-B=min(r,key=lambda t:len({(r[1]+r[3]*t)%103 for r in R}))
+F=lambda i,j:min(r,key=lambda t:len({(r[i]+r[2+i]*t)%j for r in R}))
+A=F(0,101)
+B=F(1,103)
 print(K(1,1)*K(1,-1)*K(-1,1)*K(-1,-1),(B*5151+A*5253)%10403)
