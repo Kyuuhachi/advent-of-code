27d6567b61977e9de76898f7cc25a913bdb7d2ad
Kyuuhachi <caagr98@gmail.com>
2024-12-15 10:45:29 +0100

    478

--- 24/15.py
+++ 24/15.py
@@ -2,8 +2,8 @@ a,b=open("15.in",'rb').read().split(b"\n\n")
 for*G,n in(0,1),(-1,0,1,2):
  w=a.find(10)*n+n
  r=a.find(64)*n
- B={i*n for i in range(len(a))if a[i]==79}
- W={i*n for i in range(len(a))if a[i]==35}
+ B={i*n for i,c in enumerate(a) if c==79}
+ W={i*n for i,c in enumerate(a) if c==35}
  for i in b:
   d={60:-1,62:1,94:-w,118:w,10:0}[i]
   v=set()
