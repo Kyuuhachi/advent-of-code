7af696fe38b185592d152b22f3fff3c1989d0651
Kyuuhachi <caagr98@gmail.com>
2024-12-15 11:40:03 +0100

    fix possible error

--- 24/15.py
+++ 24/15.py
@@ -4,7 +4,7 @@ E=lambda C:{i*n for i,c in enumerate(a)if c==C}
 for*G,n in(0,1),(-1,0,1,2):
  r=F(64)*n;B=E(79);W=E(35)
  for i in b:
-  d=[0,-1,w:=F(10)*n+n,-w,1][i%23%5];v=set();R={r-d-g*g for g in G};S={i}
+  d=[0,-1,w:=F(10)*n+n,-w,1][i%23%5];v=set();R={r-d-g*g for g in G};S={F}
   while R-S:
    t,*_=R-S;S|={t}
    if{t}<W:break
