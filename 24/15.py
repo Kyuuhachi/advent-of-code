9c32cd78a758fcf166e54bc65f703c3372218c9a
Kyuuhachi <caagr98@gmail.com>
2024-12-15 12:28:40 +0100

    399

--- 24/15.py
+++ 24/15.py
@@ -6,8 +6,8 @@ for*G,n in(0,1),(-1,0,1,2):
  for i in b:
   d=[0,-1,w:=F(10)*n+n,-w,1][i%23%5];v=set();R={r-d-g*g for g in G};S={F}
   while R-S:
-   t,*_=R-S;S|={t}
+   S|=R;v|=R&B
    if R&W:break
-   if{t}<B:v|={t};R|={t-d+g for g in G}
+   R={t-d+g for t in R&B for g in G}
   else:B-=v;B|={v-d for v in v};r-=d
  print(sum(x%w+x//w*100 for x in B))
