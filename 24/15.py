8d1133fb0eadee3edad9449620dd42d4be56a6d4
Kyuuhachi <caagr98@gmail.com>
2024-12-15 10:31:03 +0100

    Day 15

--- 24/15.py
+++ 24/15.py
@@ -2,31 +2,47 @@ a,b=open("15.in").read().split("\n\n")
 w=a.find('\n')+1
 D={'<':-1,'>':1,'^':-w,'v':w,'\n':0}
 
-g=list(a)
-r=g.index('@')
-for i in b:
- d=D[i]
- R=r
- while g[R+d]=='O':R+=d
- if g[R+d]!='#':
-  g[r],g[R+d],g[r+d]='.O@'
-  r+=d
-print(sum(x%w+x//w*100 for x,c in enumerate(g)if c=='O'))
-
 r=a.find('@')
 B={i for i in range(len(a))if a[i]=='O'}
+W={i for i in range(len(a))if a[i]=='#'}
 for i in b:
  if i=='\n':continue
  d=D[i]
- R=r
- while R+d in B:R+=d
- if a[R+d]!='#':
-  if r+d in B:
-   B-={r+d}
-   B|={R+d}
+ v=set()
+ R={r}
+ while R:
+  t=R.pop()
+  if t+d in W:break
+  if t+d in B:R|={t+d}
+  v|={t}
+ else:
+  v-={r}
+  B-=v
+  B|={v+d for v in v}
   r+=d
 print(sum(x%w+x//w*100 for x in B))
 
-r=a.index('@')*2
+w*=2
+D={'<':-1,'>':1,'^':-w,'v':w,'\n':0}
+r=a.find('@')*2
 B={i*2 for i in range(len(a))if a[i]=='O'}
-print(B)
+W={i*2 for i in range(len(a))if a[i]=='#'}
+for i in b:
+ if i=='\n':continue
+ d=D[i]
+ v=set()
+ R={r+d,r+d-1}
+ S={i}
+ while R-S:
+  t,*_=R-S
+  S|={t}
+  if t in W:
+   break
+  if t in B:
+   v|={t}
+   R|={t+d+g for g in[-1,0,1]}
+ else:
+  B-=v
+  B|={v+d for v in v}
+  r+=d
+print(sum(x%w+x//w*100 for x in B))
