9df8ef4405fd45e6ac81e270ca594e5ce74f972f
Kyuuhachi <caagr98@gmail.com>
2024-12-15 10:37:57 +0100

    510

--- 24/15.py
+++ 24/15.py
@@ -1,46 +1,27 @@
 a,b=open("15.in").read().split("\n\n")
-w=a.find('\n')+1
-
-r=a.find('@')
-B={i for i in range(len(a))if a[i]=='O'}
-W={i for i in range(len(a))if a[i]=='#'}
-for i in b:
- if i=='\n':continue
- d={'<':-1,'>':1,'^':-w,'v':w,'\n':0}[i]
- v=set()
- R={r}
- while R:
-  t=R.pop()
-  if t+d in W:break
-  if t+d in B:R|={t+d}
-  v|={t}
- else:
-  v-={r}
-  B-=v
-  B|={v+d for v in v}
-  r+=d
-print(sum(x%w+x//w*100 for x in B))
-
-w*=2
-r=a.find('@')*2
-B={i*2 for i in range(len(a))if a[i]=='O'}
-W={i*2 for i in range(len(a))if a[i]=='#'}
-for i in b:
- if i=='\n':continue
- d={'<':-1,'>':1,'^':-w,'v':w,'\n':0}[i]
- v=set()
- R={r+d,r+d-1}
- S={i}
- while R-S:
-  t,*_=R-S
-  S|={t}
-  if t in W:
-   break
-  if t in B:
-   v|={t}
-   R|={t+d+g for g in[-1,0,1]}
- else:
-  B-=v
-  B|={v+d for v in v}
-  r+=d
-print(sum(x%w+x//w*100 for x in B))
+def F(n,*G):
+ w=a.find('\n')*n+n
+ r=a.find('@')*n
+ B={i*n for i in range(len(a))if a[i]=='O'}
+ W={i*n for i in range(len(a))if a[i]=='#'}
+ for i in b:
+  if i=='\n':continue
+  d={'<':-1,'>':1,'^':-w,'v':w,'\n':0}[i]
+  v=set()
+  R={r+d-g*g for g in G}
+  S={i}
+  while R-S:
+   t,*_=R-S
+   S|={t}
+   if t in W:
+    break
+   if t in B:
+    v|={t}
+    R|={t+d+g for g in G}
+  else:
+   B-=v
+   B|={v+d for v in v}
+   r+=d
+ print(sum(x%w+x//w*100 for x in B))
+F(1,0)
+F(2,-1,0,1)
