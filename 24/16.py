9bf3fd7195379ccc7ec0e042c8c3f09b9e52d3d8
Kyuuhachi <caagr98@gmail.com>
2024-12-16 16:18:45 +0100

    356 but faster

--- 24/16.py
+++ 24/16.py
@@ -9,10 +9,11 @@ def P(W,P,D):
  heappush(H,(W,P,D))
 
 F=s.find
+K=1,F(10)+1
 S=F(83)
-G=[S*[(1e9*(c!=35),0)]for c in s]
+G=[2*[(1e9*(c!=35),0)]for c in s]
 G[S][1]=(1,{S})
 H=[(1,S,1)]
-while H:w,p,d=heappop(H);P(1,p+d,d);P(1,p-d,d);P(1e3,p,d^1^F(10)+1)
+while H:w,p,d=heappop(H);P(1,p+K[d],d);P(1,p-K[d],d);P(1e3,p,1-d)
 A,B=min(G[F(69)])
 print(A,len(B))
