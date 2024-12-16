8e5e43b50bc82beb9c9e71b6ea97e1e7619a92b0
Kyuuhachi <caagr98@gmail.com>
2024-12-16 16:49:52 +0100

    332

--- 24/16.py
+++ 24/16.py
@@ -1,6 +1,6 @@
 from heapq import*
 s=open("16.in",'rb').read()
-def P(W,P,D):
+def P(P,D,W=1):
  W+=w;q,m=G[P][D];o=G[p][d][1]|{P}
  if W<q:G[P][D]=W,o
  if W<=q:m|=o;heappush(H,(W,P,D))
@@ -9,6 +9,6 @@ S=F(83)
 G=[2*[(1e9*(c-35),{0})]for c in s]
 G[S][1]=1,{S}
 H=[(1,S,1)]
-while H:w,p,d=heappop(H);q=1+F(10)*d;P(1,p+q,d);P(1,p-q,d);P(1e3,p,1-d)
+while H:w,p,d=heappop(H);q=1+F(10)*d;P(p+q,d);P(p-q,d);P(p,1-d,1e3)
 A,B=min(G[F(69)])
 print(A,len(B))
