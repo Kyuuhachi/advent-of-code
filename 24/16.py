fab2d90cb3a85fdf053a7902d7370cf70624a4ec
Kyuuhachi <caagr98@gmail.com>
2024-12-16 16:47:42 +0100

    334

--- 24/16.py
+++ 24/16.py
@@ -5,11 +5,10 @@ def P(W,P,D):
  if W<q:G[P][D]=W,o
  if W<=q:m|=o;heappush(H,(W,P,D))
 F=s.find
-K=F(10)
 S=F(83)
 G=[2*[(1e9*(c-35),{0})]for c in s]
 G[S][1]=1,{S}
 H=[(1,S,1)]
-while H:w,p,d=heappop(H);P(1,p+1+K*d,d);P(1,p-1-K*d,d);P(1e3,p,1-d)
+while H:w,p,d=heappop(H);q=1+F(10)*d;P(1,p+q,d);P(1,p-q,d);P(1e3,p,1-d)
 A,B=min(G[F(69)])
 print(A,len(B))
