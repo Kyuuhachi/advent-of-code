3e0acbec04efaf927c64c3e4b2b4af30ad9b6d49
Kyuuhachi <caagr98@gmail.com>
2024-12-16 15:43:32 +0100

    404, but faster

--- 24/16.py
+++ 24/16.py
@@ -4,21 +4,21 @@ w=s.find(10)+1
 S=s.find(83)
 E=s.find(69)
 
-W=[1,w,-1,-w]
-r=lambda n:W[W.index(n)-1]
-def P(W,p,d):heappush(H,(W+w,l|{p},p,d))
+W={1:w,w:-1,-1:-w,-w:1}
+def P(W,P,D):
+ q,m=G[P][D]
+ if W+w>q:return
+ if W+w<q:G[P][D]=W+w,G[p][d][1]|{P}
+ else:m|=G[p][d][1]|{P}
+ heappush(H,(W+w,P,D))
 
-D=[S*[(1e9*(c!=35),0)]for c in s]
-d=D[S][1]=(1,{S})
-H=[(*d,S,1)]
+G=[S*[(1e9*(c!=35),0)]for c in s]
+G[S][1]=(1,{S})
+H=[(1,S,1)]
 while H:
- w,l,p,d=heappop(H)
- q,m=D[p][d]
- if w>q:continue
- if w<q:D[p][d]=w,l
- else:m|=l
+ w,p,d=heappop(H)
  P(1,p+d,d)
- P(1e3,p,r(d))
- P(1e3,p,r(-d))
-A,B=min(D[E])
+ P(1e3,p,W[d])
+ P(1e3,p,W[-d])
+A,B=min(G[E])
 print(A,len(B))
