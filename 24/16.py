f12e3a4ecf61a4e7ca2b525804920502e2142a55
Kyuuhachi <caagr98@gmail.com>
2024-12-16 16:30:09 +0100

    349

--- 24/16.py
+++ 24/16.py
@@ -8,7 +8,7 @@ def P(W,P,D):
 F=s.find
 K=1,F(10)+1
 S=F(83)
-G=[2*[(1e9*(c!=35),{0})]for c in s]
+G=[2*[(1e9*(c-35),{0})]for c in s]
 G[S][1]=(1,{S})
 H=[(1,S,1)]
 while H:w,p,d=heappop(H);P(1,p+K[d],d);P(1,p-K[d],d);P(1e3,p,1-d)
