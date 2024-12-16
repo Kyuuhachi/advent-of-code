7b96d896012d82f43c845389f6b1b7ed17b45910
Kyuuhachi <caagr98@gmail.com>
2024-12-16 16:07:30 +0100

    346

--- 24/16.py
+++ 24/16.py
@@ -13,10 +13,6 @@ S=F(83)
 G=[S*[(1e9*(c!=35),0)]for c in s]
 G[S][1]=(1,{S})
 H=[(1,S,1)]
-while H:
- w,p,d=heappop(H)
- P(1,p+d,d)
- P(1,p-d,d)
- P(1e3,p,d^1^F(10)+1)
+while H:w,p,d=heappop(H);P(1,p+d,d);P(1,p-d,d);P(1e3,p,d^1^F(10)+1)
 A,B=min(G[F(69)])
 print(A,len(B))
