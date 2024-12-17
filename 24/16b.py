0c1bd7893f10bd955471d183752704d6ec7cfda5
Kyuuhachi <caagr98@gmail.com>
2024-12-18 00:24:54 +0100

    409

--- 24/16b.py
+++ 24/16b.py
@@ -1,10 +1,10 @@
 s=open("16.in").read()
 G=[2*[[-2,1e9][c!='#']]for c in s]
 S=1,s.find('\n')+1
-def A(p,d,g,k):
- while G[p][d]>g:G[p][d]=g;F.append((p,1-d,g+1000));p+=k;g+=1
 F=[(s.find('S'),0,1)]
-for p,d,g in F:k=S[d];A(p,d,g,-k);A(p+k,d,g+1,k)
+for p,d,g in F:
+ for p,d,g,k in(p,d,g,k:=S[d]),(p-k,d,g+1,-k):
+  while G[p][d]>g:G[p][d]=g;F.append((p,1-d,g+1000));p+=k;g+=1
 e=s.find('E')
 P=[(e,G[e][0]>G[e][1])]
 for p,d in P:
