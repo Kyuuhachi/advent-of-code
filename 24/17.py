bd29342f65d4bb4612f49df59e91f502f60c6c2f
Kyuuhachi <caagr98@gmail.com>
2024-12-17 23:07:39 +0100

    300

--- 24/17.py
+++ 24/17.py
@@ -4,6 +4,6 @@ N,M=[C[i+1]for i in range(0,16,2)if C[i]==1]
 D=lambda a:(b:=a&7^N)^(a>>b)&7^M
 o=[]
 while a:o+=[D(a)];a>>=3
-print(','.join(map(str,o)))
+print(*o,sep=',')
 F=lambda A,c:(k for b in range(8)if D(a:=A<<3|b)==c[-1]for k in F(a, c[:-1]))if c else[A]
 print(next(F(0,C)))
