4e6a96f2886734081edd08de85b9d03857a27f20
Kyuuhachi <caagr98@gmail.com>
2024-12-17 23:08:57 +0100

    294

--- 24/17.py
+++ 24/17.py
@@ -5,5 +5,5 @@ D=lambda a:(b:=a&7^N)^(a>>b)&7^M
 o=[]
 while a:o+=[D(a)];a>>=3
 print(*o,sep=',')
-F=lambda A,c:(k for b in range(8)if D(a:=A<<3|b)==c[-1]for k in F(a, c[:-1]))if c else[A]
-print(next(F(0,C)))
+F=lambda A,c:sum((F(a, c[:-1])for b in range(8)if D(a:=A<<3|b)==c[-1]),[])if c else[A]
+print(F(0,C)[0])
