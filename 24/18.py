d54da3f5ff9060bd6a8d08d4ba004b7e7e5516cc
Kyuuhachi <caagr98@gmail.com>
2024-12-18 21:42:31 +0100

    314

--- 24/18.py
+++ 24/18.py
@@ -1,12 +1,10 @@
 I=[(*map(int,s[:-1].split(',')),) for s in open("18.in")]
 def F(n):
- P=[[0]*3]
- S={*I[:n]}
+ P=[[0]*3];S={*I[:n]}
  for x,y,s in P:
   if{v:=(x,y)}-S and 0<=x<71>y>=0:S|={v};P+=(x-1,y,w:=s+1),(x+1,y,w),(x,y-1,w),(x,y+1,w)
   if(70,70)==v:return s
 print(F(m:=1024))
-
 M=len(I)
 while m<M:
  if F(q:=m+M>>1):m=q+1
