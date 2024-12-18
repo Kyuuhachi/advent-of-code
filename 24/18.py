8f51a05df28b1f382c66cc40406028109fa06c66
Kyuuhachi <caagr98@gmail.com>
2024-12-18 21:13:48 +0100

    344

--- 24/18.py
+++ 24/18.py
@@ -2,10 +2,9 @@ I=[(*map(int,s[:-1].split(',')),) for s in open("18.in")]
 def F(n):
  P=[(0,0,0)]
  S={*I[:n]}
- W=70
  for x,y,s in P:
-  if(x,y)==(W,W):return s
-  if{(x,y)}-S and 0<=x<=W and 0<=y<=W:
+  if(x,y)==(70,70):return s
+  if{(x,y)}-S and 0<=x<71 and 0<=y<71:
    S|={(x,y)}
    P+=[(x-1,y,s+1),(x+1,y,s+1),(x,y-1,s+1),(x,y+1,s+1)]
 print(F(m:=1024))
