e08480f8ca23c3a067137fedf5de125fd1d2438c
Kyuuhachi <caagr98@gmail.com>
2024-12-18 02:11:11 +0100

    367

--- 24/16b.py
+++ 24/16b.py
@@ -6,8 +6,7 @@ F=[(f('S'),0,1)]
 for p,d,g in F:
  for p,g,k in(p,g,k:=S[d]),(p-k,g+1,-k):
   while G[p][d]>g:G[p][d]=g;g+=1;F+=(p,1-d,g+999),;p+=k
-e=f('E')
-T=G[e]
+T=G[e:=f('E')]
 P=[(e,T[0]>T[1])]
 for p,d in P:g=G[p][d]-1;P+=[(p,1-d)][G[p][1-d]-g+999:]+[(p+k,d)for k in[-S[d],S[d]]if G[p+k][d]==g]
 print(min(T),len(dict(P)))
