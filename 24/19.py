55af0272b338bdccdf4124e3686caf7e9a713f1b
Kyuuhachi <caagr98@gmail.com>
2024-12-19 19:17:28 +0100

    191

--- 24/19.py
+++ 24/19.py
@@ -1,4 +1,5 @@
 import functools as F
 G,_,*S=open("19.in")
 f=F.cache(lambda s:sum(f(s[len(g):])for g in G[:-1].split(", ")if s[:len(g)]==g)if s else 1)
-print(sum(0!=f(s[:-1])for s in S),sum(f(s[:-1])for s in S))
+v=[f(s[:-1])for s in S]
+print(sum(map(bool,v)),sum(v))
