59740ce3c18aa55eb61b6dc7d596521ea9a8976c
Kyuuhachi <caagr98@gmail.com>
2024-12-19 18:44:36 +0100

    259→255

--- 24/19.py
+++ 24/19.py
@@ -1,13 +1,12 @@
 import functools as F
-s=open("19.in").read().splitlines()
-G=s[0].split(", ")
+S=open("19.in").read().splitlines()
 @F.cache
 def f(s):
  if s == "": return 1
  n = 0
- for g in G:
+ for g in S[0].split(", "):
   if s.startswith(g):
    n += f(s[len(g):])
  return n
-print(sum(f(s)!=0 for s in s[2:]))
-print(sum(f(s) for s in s[2:]))
+print(sum(f(s)!=0 for s in S[2:]))
+print(sum(f(s) for s in S[2:]))
