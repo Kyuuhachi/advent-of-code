c16307d5e4c38af210cacea03e6a31279845234c
Kyuuhachi <caagr98@gmail.com>
2024-12-19 18:59:06 +0100

    201

--- 24/19.py
+++ 24/19.py
@@ -1,4 +1,4 @@
 import functools as F
 G,_,*S=open("19.in").read().splitlines()
-f=F.cache(lambda s:sum(f(s[len(g):])for g in G.split(", ")if s.startswith(g))if s else 1)
+f=F.cache(lambda s:sum(f(s[len(g):])for g in G.split(", ")if s[:len(g)]==g)if s else 1)
 print(sum(0!=f(s)for s in S),sum(f(s)for s in S))
