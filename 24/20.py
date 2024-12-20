3de49dfa2a973b41650ec1b799ecf7a7b29e0733
Kyuuhachi <caagr98@gmail.com>
2024-12-20 23:59:07 +0100

    293

--- 24/20.py
+++ 24/20.py
@@ -8,6 +8,6 @@ for x in[-1,w:=f('\n')+1,1,-w]*w*w:
 n=0
 for a in M:
  for X in range(-20,21):
-  if(a+X)//w==a//w:
+  if-1<a%w+X<w:
    for Y in range(r:=abs(X)-20,1-r):n+=M[a]-M.get(b:=a+X+Y*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)>99
 print(n)
