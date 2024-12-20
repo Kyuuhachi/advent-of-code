f80bd0c8fbf86cc24bfe6be073bb40f84fffed04
Kyuuhachi <caagr98@gmail.com>
2024-12-21 00:01:32 +0100

    292

--- 24/20.py
+++ 24/20.py
@@ -4,10 +4,10 @@ p,=M={f('S'):0}
 P=0
 for x in[-1,w:=f('\n')+1,1,-w]*w*w:
  if s<s[p+x]and P+x:P=x;p+=x;M[p]=len(M)
-
+R=range
 n=0
 for a in M:
- for X in range(-20,21):
+ for X in R(-20,21):
   if-1<a%w+X<w:
-   for Y in range(r:=abs(X)-20,1-r):n+=M[a]-M.get(b:=a+X+Y*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)>99
+   for Y in R(r:=abs(X)-20,1-r):n+=M[a]-M.get(b:=a+X+Y*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)>99
 print(n)
