c2b0183d4442d88be3576465a0e6b2d6f857f9d9
Kyuuhachi <caagr98@gmail.com>
2024-12-20 23:45:03 +0100

    382→382, but faster

--- 24/20.py
+++ 24/20.py
@@ -11,11 +11,9 @@ for a in M:
  k += 1
  if k % 10 == 0:print(k,'/',len(M))
  for dx in range(-20,21):
-  for dy in range(-20,21):
-   b = a + dx + dy * w
-   if b in M:
-    d=abs(a%w-b%w)+abs(a//w-b//w)
-    if d<=20:
-     d=M[a]-M[b]-d
-     if d>=100:n+=1
+  if(a+dx)//w!=a//w:continue
+  r=20-abs(dx)
+  for dy in range(-r,r+1):
+   d=M[a]-M.get(b:=a+dx+dy*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)
+   if d>=100:n+=1
 print(n)
