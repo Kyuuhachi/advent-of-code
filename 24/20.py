af8eef3414c8cfff1426308831dfcb3a22980fde
Kyuuhachi <caagr98@gmail.com>
2024-12-20 23:32:38 +0100

    48s→10s

--- 24/20.py
+++ 24/20.py
@@ -10,10 +10,12 @@ k = 0
 for a in M:
  k += 1
  if k % 10 == 0:print(k,'/',len(M))
- for b in M:
-  d=abs(a%w-b%w)+abs(a//w-b//w)
-  if d<=20:
-   d=M[a]-M[b]-d
-   # if d>=50:print(d)
-   if d>=100:n+=1
+ for dx in range(-20,21):
+  for dy in range(-20,21):
+   b = a + dx + dy * w
+   if b in M:
+    d=abs(a%w-b%w)+abs(a//w-b//w)
+    if d<=20:
+     d=M[a]-M[b]-d
+     if d>=100:n+=1
 print(n)
