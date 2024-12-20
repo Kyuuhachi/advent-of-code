742eaf2ec6cdd19159bd7015df14f25591abfd78
Kyuuhachi <caagr98@gmail.com>
2024-12-21 00:08:34 +0100

    301

--- 24/20.py
+++ 24/20.py
@@ -6,9 +6,10 @@ for x in[-1,w:=f('\n')+1,1,-w]*w*w:
  if s<s[p+x]and P+x:P=x;p+=x;M[p]=len(M)
 R=range
 for x in 2,20:
- n=0
- for a in M:
-  for X in R(-x,x+1):
-   if-1<a%w+X<w:
-    for Y in R(r:=abs(X)-x,1-r):n+=M[a]-M.get(b:=a+X+Y*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)>99
- print(n)
+ print(sum(
+M[a]-M.get(b:=a+X+Y*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)>99
+for a in M
+for X in R(-x,x+1)
+if-1<a%w+X<w
+for Y in R(abs(X)-x,1-abs(X)+x)
+ ))
