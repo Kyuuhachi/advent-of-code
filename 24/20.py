cfb892edd3e36556da67ba84a83e252717ac4e19
Kyuuhachi <caagr98@gmail.com>
2024-12-21 00:02:25 +0100

    Add both parts

--- 24/20.py
+++ 24/20.py
@@ -5,9 +5,10 @@ P=0
 for x in[-1,w:=f('\n')+1,1,-w]*w*w:
  if s<s[p+x]and P+x:P=x;p+=x;M[p]=len(M)
 R=range
-n=0
-for a in M:
- for X in R(-20,21):
-  if-1<a%w+X<w:
-   for Y in R(r:=abs(X)-20,1-r):n+=M[a]-M.get(b:=a+X+Y*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)>99
-print(n)
+for x in 2,20:
+ n=0
+ for a in M:
+  for X in R(-x,x+1):
+   if-1<a%w+X<w:
+    for Y in R(r:=abs(X)-x,1-r):n+=M[a]-M.get(b:=a+X+Y*w,1e9)-abs(a%w-b%w)-abs(a//w-b//w)>99
+ print(n)
