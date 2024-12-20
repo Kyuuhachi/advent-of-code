a951dd4142730f9256e088d9dec24d8b4d87b157
Kyuuhachi <caagr98@gmail.com>
2024-12-21 00:19:53 +0100

    More curseder

--- 24/20.py
+++ 24/20.py
@@ -7,4 +7,4 @@ for x in[-1,w:=f('\n')+1,1,-w]*w*w:
 R=range
 A=abs
 for x in 2,20:print(sum(M[a]-M.get(b:=a+X+Y*w,1e9)>99+A(a%w-b%w)+A(a//w-b//w)for a in M
-for X in R(-x,x+1)for Y in R(A(X)-x,1-A(X)+x)if-1<a%w+X<w))
+for X in R(-x,x+1)for Y in R(A(X)-x,1-A(X)+x)if~X<a%w<w-X))
