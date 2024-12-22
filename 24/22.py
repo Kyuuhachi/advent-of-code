eeea77a5fee3a153b11560b129d25c669027a5d8
Kyuuhachi <caagr98@gmail.com>
2024-12-22 14:55:06 +0100

    272, using running hash

--- 24/22.py
+++ 24/22.py
@@ -3,8 +3,8 @@ def H(x):x^=(x<<6)%V;x^=x>>5;return(x^x<<11)%V
 G={}
 n=0
 for i in open("22.in"):
- i=int(i[:-1]);j=[i%10]
- for _ in range(2000):i=H(i);j[:0]=i%10,
+ i=int(i[:-1]);j=i%10,;p=i%10;w=[]
+ for _ in range(2000):i=H(i);j+=i%10-p,;p=i%10;w+=(j[-4:],p),
  n+=i
- for k,v in{(b-a,c-b,d-c,e-d):e for a,b,c,d,e in zip(j,j[1:],j[2:],j[3:],j[4:])}.items():G[k]=G.get(k,0)+v
+ for k,v in dict(w[3:][::-1]).items():G[k]=G.get(k,0)+v
 print(n,max(G.values()))
