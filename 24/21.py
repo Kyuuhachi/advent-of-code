d91e192707666d3f43c3de584454ffaff54af0cb
Kyuuhachi <caagr98@gmail.com>
2024-12-22 11:31:55 +0100

    407

--- 24/21.py
+++ 24/21.py
@@ -6,10 +6,10 @@ Q=' ^A<v>'
 def G(s,N,B):
  if N==0:return len(s)
  o=0
- p=B.find('A')
- H=B.find(' ')
+ f=B.find;p=f('A')
+ H=f(' ')
  for c in s:
-  P=B.find(c)
+  P=f(c)
   x=P%3-p%3;x='>'*x+'<'*-x
   y=P//3-p//3;y='v'*y+'^'*-y
   o+=min([G(x+y+'A',N-1,Q)][:P%3!=H%3 or p//3!=H//3]+[G(y+x+'A',N-1,Q)][:p%3!=H%3 or P//3!=H//3])
