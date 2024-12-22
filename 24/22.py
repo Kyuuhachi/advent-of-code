2d97858550f98fda4aa540ec33941e236269d8de
Kyuuhachi <caagr98@gmail.com>
2024-12-22 14:22:05 +0100

    291

--- 24/22.py
+++ 24/22.py
@@ -4,7 +4,7 @@ G={}
 n=0
 for i in open("22.in"):
  i=int(i[:-1]);j=[i%10]
- for _ in range(2000):i=H(i);j+=i%10,
- n+=i;j=j[::-1]
+ for _ in range(2000):i=H(i);j[:0]=i%10,
+ n+=i
  for k,v in{(b-a,c-b,d-c,e-d):e for a,b,c,d,e in zip(j,j[1:],j[2:],j[3:],j[4:])}.items():G[k]=G.get(k,0)+v
 print(n,max(G.values()))
