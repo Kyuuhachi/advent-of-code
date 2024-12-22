70c975f944e10e1968eb4564eedc4b0d0bff7856
Kyuuhachi <caagr98@gmail.com>
2024-12-22 15:01:23 +0100

    267

--- 24/22.py
+++ 24/22.py
@@ -4,7 +4,7 @@ G={}
 n=0
 for i in open("22.in"):
  i=int(i[:-1]);j=p=i%10;w=[]
- for _ in range(2000):i=H(i);j=j*18%18**4+i%10-p;p=i%10;w+=(j,p),
+ for _ in range(2000):i=H(i);w+=(j:=j*18%18**4-p+(p:=i%10),p),
  n+=i
  for k,v in dict(w[3:][::-1]).items():G[k]=G.get(k,0)+v
 print(n,max(G.values()))
