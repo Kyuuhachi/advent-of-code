6af8e431524485607a55ebd7b803d625e0c7e90f
Kyuuhachi <caagr98@gmail.com>
2024-12-22 14:57:00 +0100

    269

--- 24/22.py
+++ 24/22.py
@@ -3,7 +3,7 @@ def H(x):x^=(x<<6)%V;x^=x>>5;return(x^x<<11)%V
 G={}
 n=0
 for i in open("22.in"):
- i=int(i[:-1]);j=i%10,;p=i%10;w=[]
+ i=int(i[:-1]);p=i%10;j=p,;w=[]
  for _ in range(2000):i=H(i);j+=i%10-p,;p=i%10;w+=(j[-4:],p),
  n+=i
  for k,v in dict(w[3:][::-1]).items():G[k]=G.get(k,0)+v
