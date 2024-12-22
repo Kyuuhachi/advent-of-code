b17dc2304b04ea13b12f3b56dc3107b707a183a9
Kyuuhachi <caagr98@gmail.com>
2024-12-22 15:15:35 +0100

    228

--- 24/22.py
+++ 24/22.py
@@ -3,7 +3,7 @@ G=[0]*18**4
 n=0
 for i in open("22.in"):
  i=int(i[:-1]);j=p=i%10;w=[]
- for _ in range(2000):i^=(i<<6)%V;i^=i>>5;i^=(i<<11)%V;w+=(j:=j*18%18**4-p+(p:=i%10),p),
+ for _ in range(2000):i^=i*64%V;i^=i>>5;i^=i*2048%V;w+=(j:=j*18%18**4-p+(p:=i%10),p),
  n+=i
  for k,v in dict(w[:2:-1]).items():G[k]+=v
 print(n,max(G))
