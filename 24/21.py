99792d14fb6750ae7e54944d7a2f68987b17de25
Kyuuhachi <caagr98@gmail.com>
2024-12-22 11:36:54 +0100

    393

--- 24/21.py
+++ 24/21.py
@@ -6,13 +6,12 @@ Q=' ^A<v>'
 def G(s,N,B):
  if N==0:return len(s)
  o=0
- f=B.find;p=f('A')
- H=f(' ')
+ f=B.find;p=f('A');H=f(' ')//3
  for c in s:
   P=f(c)
   x=P%3-p%3;x='>'*x+'<'*-x
   y=P//3-p//3;y='v'*y+'^'*-y
-  o+=min([G(x+y+'A',N-1,Q)][:P%3!=H%3 or p//3!=H//3]+[G(y+x+'A',N-1,Q)][:p%3!=H%3 or P//3!=H//3])
+  o+=min([G(x+y+'A',N-1,Q)][:P%3 or p//3!=H]+[G(y+x+'A',N-1,Q)][:p%3 or P//3!=H])
   p=P
  return o
 for N in 3,26:print(sum(int(i[:-1])*G(i,N,'789456123 0A')for i in s))
