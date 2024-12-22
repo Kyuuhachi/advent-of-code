aab517658ac025988017120d49477b5630973202
Kyuuhachi <caagr98@gmail.com>
2024-12-22 13:43:45 +0100

    326

--- 24/22.py
+++ 24/22.py
@@ -6,8 +6,8 @@ def H(x):
 
 G={}
 n=0
-for i in map(int,open("22.in").read().split()):
- j=[i%10]
+for i in open("22.in"):
+ i=int(i[:-1]);j=[i%10]
  for _ in range(2000):i=H(i);j+=i%10,
  n+=i;j=j[::-1]
  for k,v in{(b-a,c-b,d-c,e-d):e for a,b,c,d,e in zip(j,j[1:],j[2:],j[3:],j[4:])}.items():G[k]=G.get(k,0)+v
