fb9c4f155603439e34d6b72818f00066fc981bfc
Kyuuhachi <caagr98@gmail.com>
2024-12-22 13:01:49 +0100

    way faster

--- 24/22.py
+++ 24/22.py
@@ -5,8 +5,7 @@ def H(x):
  x ^= x << 11 & 0xFFFFFF
  return x
 
-
-o = []
+G=[0]*19**4
 n = 0
 for i in s:
  j = [i%10]
@@ -15,20 +14,11 @@ for i in s:
   j.append(i%10)
  n += i
  g = {}
+ j=j[::-1]
  for a,b,c,d,e in zip(j,j[1:],j[2:],j[3:],j[4:]):
   p = (b-a,c-b,d-c,e-d)
-  if p not in g: g[p] = e
- o.append(g)
+  p = sum(p*19**n for n,p in enumerate(p))
+  g[p] = e
+ for k,v in g.items(): G[k] += v
 print(n)
-
-S={K for k in o for K in k}
-
-v = 0
-for i,D in enumerate(S):
- if i % 100 == 0: print(i, '/', len(S))
- banana = 0
- for k in o:
-  banana += k.get(D, 0)
- v = max(v,banana)
-print(v)
-# 1450 too high
+print(max(G))
