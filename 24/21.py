2579209741c1091980ba58fd77034afe290b2d1d
Kyuuhachi <caagr98@gmail.com>
2024-12-22 11:30:53 +0100

    413

--- 24/21.py
+++ 24/21.py
@@ -1,5 +1,6 @@
 import functools as F
 s=open("21.in").read().split()
+Q=' ^A<v>'
 
 @F.cache
 def G(s,N,B):
@@ -11,7 +12,7 @@ def G(s,N,B):
   P=B.find(c)
   x=P%3-p%3;x='>'*x+'<'*-x
   y=P//3-p//3;y='v'*y+'^'*-y
-  o+=min([G(x+y+'A',N-1,' ^A<v>')][:P%3!=H%3 or p//3!=H//3]+[G(y+x+'A',N-1,' ^A<v>')][:p%3!=H%3 or P//3!=H//3])
+  o+=min([G(x+y+'A',N-1,Q)][:P%3!=H%3 or p//3!=H//3]+[G(y+x+'A',N-1,Q)][:p%3!=H%3 or P//3!=H//3])
   p=P
  return o
 for N in 3,26:print(sum(int(i[:-1])*G(i,N,'789456123 0A')for i in s))
