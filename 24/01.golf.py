a0d5835bcbf3482cb5fc36b449320239941dd66f
Kyuuhachi <caagr98@gmail.com>
2024-12-22 15:28:04 +0100

    Remove ungolfed day 1

--- 24/01.golf.py
+++
@@ -1,2 +0,0 @@
-a,b=[sorted(int(x.split()[i])for x in open("01.in"))for i in(0,1)]
-print(sum(abs(q-w)+1j*q*b.count(q)for q,w in zip(a,b)))
--- 24/01.py
+++ 24/01.py
@@ -1,10 +1,2 @@
-from collections import Counter
-a, b = [], []
-for line in open("01.in").read().splitlines():
-	v1, v2 = line.split()
-	a.append(int(v1))
-	b.append(int(v2))
-
-print(sum(abs(v1 - v2) for v1, v2 in zip(sorted(a), sorted(b))))
-counts = Counter(b)
-print(sum(v1 * counts[v1] for v1 in a))
+a,b=[sorted(int(x.split()[i])for x in open("01.in"))for i in(0,1)]
+print(sum(abs(q-w)+1j*q*b.count(q)for q,w in zip(a,b)))
