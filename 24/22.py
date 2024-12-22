17e68befb20b52e933a83234ec634af248097ad2
Kyuuhachi <caagr98@gmail.com>
2024-12-22 14:58:21 +0100

    270, but 3x as fast

--- 24/22.py
+++ 24/22.py
@@ -3,8 +3,8 @@ def H(x):x^=(x<<6)%V;x^=x>>5;return(x^x<<11)%V
 G={}
 n=0
 for i in open("22.in"):
- i=int(i[:-1]);p=i%10;j=p,;w=[]
- for _ in range(2000):i=H(i);j+=i%10-p,;p=i%10;w+=(j[-4:],p),
+ i=int(i[:-1]);j=p=i%10;w=[]
+ for _ in range(2000):i=H(i);j=j*18%18**4+i%10-p;p=i%10;w+=(j,p),
  n+=i
  for k,v in dict(w[3:][::-1]).items():G[k]=G.get(k,0)+v
 print(n,max(G.values()))
