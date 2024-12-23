fd40fb1fbbebe24a9ede10e310a63d3587773093
Kyuuhachi <caagr98@gmail.com>
2024-12-23 11:31:56 +0100

    323

--- 24/23.py
+++ 24/23.py
@@ -1,8 +1,9 @@
 g={}
 for l in open("23.in"):
  a,b=sorted([l[:2],l[3:5]])
- g.setdefault(a,set()).add(b)
- g.setdefault(b,set())
+ S=g.setdefault
+ S(a,set()).add(b)
+ S(b,set())
 
 print(sum('t'in{a[0],b[0],c[0]}for a in g for b in g[a]for c in g[a]&g[b]))
 
