9baffc2a6e901cc40a7a19c298bd4ff6d8da2306
Kyuuhachi <caagr98@gmail.com>
2024-12-23 11:32:40 +0100

    316

--- 24/23.py
+++ 24/23.py
@@ -1,12 +1,6 @@
 g={}
-for l in open("23.in"):
- a,b=sorted([l[:2],l[3:5]])
- S=g.setdefault
- S(a,set()).add(b)
- S(b,set())
-
+for l in open("23.in"):a,b=sorted([l[:2],l[3:5]]);S=g.setdefault;S(a,set()).add(b);S(b,set())
 print(sum('t'in{a[0],b[0],c[0]}for a in g for b in g[a]for c in g[a]&g[b]))
-
 import functools as F
 a=F.cache(lambda w,s:max((a(w+(x,),s&g[x])for x in s),key=len)if s else w)
 print(','.join(sorted(a((),frozenset(g)))))
