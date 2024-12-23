525b7b0ff8957ee83887f1d28c35ec9b98df3c73
Kyuuhachi <caagr98@gmail.com>
2024-12-23 11:31:27 +0100

    329

--- 24/23.py
+++ 24/23.py
@@ -7,6 +7,5 @@ for l in open("23.in"):
 print(sum('t'in{a[0],b[0],c[0]}for a in g for b in g[a]for c in g[a]&g[b]))
 
 import functools as F
-@F.cache
-def a(w,s):return max((a((w+(x,)),s&g[x])for x in s), key=len)if s else w
+a=F.cache(lambda w,s:max((a(w+(x,),s&g[x])for x in s),key=len)if s else w)
 print(','.join(sorted(a((),frozenset(g)))))
