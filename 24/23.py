30c7b9ae7c2778c4dc9f82a9c8623afb76b289b0
Kyuuhachi <caagr98@gmail.com>
2024-12-23 11:34:15 +0100

    308

--- 24/23.py
+++ 24/23.py
@@ -2,6 +2,5 @@ import functools as F
 S=sorted
 g={}
 for l in open("23.in"):a,b=S([l[:2],l[3:5]]);s=g.setdefault;s(a,set()).add(b);s(b,set())
-print(sum('t'in{a[0],b[0],c[0]}for a in g for b in g[a]for c in g[a]&g[b]))
 a=F.cache(lambda w,s:max((a(w+(x,),s&g[x])for x in s),key=len)if s else w)
-print(','.join(S(a((),frozenset(g)))))
+print(sum('t'in{a[0],b[0],c[0]}for a in g for b in g[a]for c in g[a]&g[b]),','.join(S(a((),frozenset(g)))))
