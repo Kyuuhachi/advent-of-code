23f9492c3a5f6e5bf327287e66b6a177acc484df
Kyuuhachi <caagr98@gmail.com>
2024-12-23 12:14:24 +0100

    252

--- 24/23.py
+++ 24/23.py
@@ -1,4 +1,4 @@
 g={}
 for l in open("23.in"):a,b=sorted([l[:2],l[3:5]]);s=g.setdefault;s(a,{b}).add(b);s(b,set())
-a=lambda s,*w:max((a(s&g[x],*w,x)for x in s),key=len,default=w)
+a=lambda s:max(([x]+a(s&g[x])for x in s),key=len,default=[])
 print(sum('t'in{a[0],b[0],c[0]}for a in g for b in g[a]for c in g[a]&g[b]),','.join(a({*g})))
