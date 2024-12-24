32b08f06d01887d9571a37d3d6d8776ad19e4b66
Kyuuhachi <caagr98@gmail.com>
2024-12-24 22:28:58 +0100

    460→426

--- 24/24.py
+++ 24/24.py
@@ -1,23 +1,23 @@
-B={}
+A,X,O={},{},{}
 for l in open("24.in"):
  match l.split():
-  case [a,b,c,d,e]:B[b,a,c]=B[b,c,a]=e
+  case [a,b,c,d,e]:G=globals()[b[0]];G[a,c]=G[c,a]=e
 
 m=[]
 K='x00','y00'
-c,p=B['XOR',*K],B['AND',*K]
+c,p=X[K],A[K]
 assert c == 'z00', c
 n=1
 while 1:
  N="%02d"%n
  K='x'+N,'y'+N
- if('XOR',*K)not in B:break
+ if K not in X:break
  if p[0]=='z':c,p=p,c;m+=[c,p]
- a,b=B['XOR',*K],B['AND',*K]
- if('XOR',p,b)in B:a,b=b,a;m+=[a,b]
- c,d=B['XOR',p,a],B['AND',p,a]
+ a,b=X[K],A[K]
+ if(p,b)in X:a,b=b,a;m+=[a,b]
+ c,d=X[p,a],A[p,a]
  if b[0]=='z':c,b=b,c;m+=[c,b]
  if d[0]=='z':c,d=d,c;m+=[c,d]
- p=B['OR',d,b]
+ p=O[d,b]
  n+=1
 print(','.join(sorted(m)))
