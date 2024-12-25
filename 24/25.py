a3139a3ec588cf9017051c97c12938baae7712a5
Kyuuhachi <caagr98@gmail.com>
2024-12-25 11:31:11 +0100

    134

--- 24/25.py
+++ 24/25.py
@@ -1,2 +1,2 @@
 K=[sum((p=='#')<<i for i,p in enumerate(s))for s in open("25.in").read().split("\n\n")]
-print(sum((a>b)&(a&b==0)for a in K for b in K))
+print(sum(a&1&(a&b==0)for a in K for b in K))
