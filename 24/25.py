dfc86a08d86f4443ff75019f7769e7a38ef76375
Kyuuhachi <caagr98@gmail.com>
2024-12-25 11:29:15 +0100

    147

--- 24/25.py
+++ 24/25.py
@@ -1,6 +1,4 @@
 K=[]
 for s in open("25.in").read().split("\n\n"):
- g=0
- for i,p in enumerate(s):g|=(p=='#')<<i
- K+=[g]
+ K+=[sum((p=='#')<<i for i,p in enumerate(s))]
 print(sum(a>b and not a&b for a in K for b in K))
