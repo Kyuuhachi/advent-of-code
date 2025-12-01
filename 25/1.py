68c0cc077a2ce8083014f63d984f329f30bd2f07
Kyuuhachi <caagr98@gmail.com>
2025-12-01 23:42:31 +0100

    116, rename to less random

--- 25/1.py
+++ 25/1.py
@@ -1,9 +1,9 @@
 v=50
-w=u=0
-for l in open(0):
-	t=l<'R'
-	k=v-t
-	v+=int('-'*t+l[1:])
-	w+=v%100<1
-	u+=abs((v-t)//100-k//100)
-print(w,u)
+n=m=0
+for L in open(0):
+	s=L<'R'
+	V=v-s
+	v+=int('-'*s+L[1:])
+	n+=v%100<1
+	m+=abs((v-s)//100-V//100)
+print(n,m)
