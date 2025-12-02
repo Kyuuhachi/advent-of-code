cbc2c008f1ca824f68b3cb75fe9a4b25624bb9d8
Kyuuhachi <caagr98@gmail.com>
2025-12-02 08:18:14 +0100

    226

---
+++ 25/2.py
@@ -0,0 +1,13 @@
+n=m=0
+for L in open(0).read()[:-1].split(','):
+ A,B=map(int,L.split('-'))
+ S={0}
+ for i in range(2,10):
+  for j in range(B):
+   j=int(str(j)*i)
+   if j<A:continue
+   if j>B:break
+   n+=j*(i==2)
+   S|={j}
+ m+=sum(S)
+print(n,m)
