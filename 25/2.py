19fb44d2b480e33b558eef707d8bc2cb812d4bc3
Kyuuhachi <caagr98@gmail.com>
2025-12-02 08:28:50 +0100

    207
    I'm pretty sure int(str(i)*m) >= i**m, so this is fine

--- 25/2.py
+++ 25/2.py
@@ -2,10 +2,9 @@ S={n:=0}
 for L in open(0).read()[:-1].split(','):
  A,B=map(int,L.split('-'))
  for i in range(2,10):
-  for j in range(B):
+  for j in range(int(B**(1/i))):
    j=int(str(j)*i)
-   j*=j>=A
-   if j>B:break
+   j*=A<=j<=B
    n+=j*(i==2)
    S|={j}
 print(n,sum(S))
