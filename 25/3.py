5e5cf8645bbea0d7f81ce5c6ab0ea0463557a01a
Kyuuhachi <caagr98@gmail.com>
2025-12-03 08:08:22 +0100

    still 155, but possibly optimizable

--- 25/3.py
+++ 25/3.py
@@ -1,5 +1,4 @@
-def F(M,i=-1):
- for k in range(M,-1,-1):n[M>6]+=int(L[i:=max(range(i+1,len(L)-k-1),key=lambda i:L[i])]+'0'*k)
+def F(M,i=-1):n[M>6]+=int(''.join(L[i:=max(range(i+1,len(L)-k-1),key=lambda i:L[i])]for k in range(M,-1,-1)))
 n=[0,0]
 for L in open(0):F(1);F(11)
 print(n)
