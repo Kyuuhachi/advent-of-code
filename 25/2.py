c1348267223b77db3a9dbcd5e078168d1e7ef120
Kyuuhachi <caagr98@gmail.com>
2025-12-03 19:43:50 +0100

    188

--- 25/2.py
+++ 25/2.py
@@ -1,8 +1,5 @@
-S={n:=0}
+Q=S,T=[{0},{0}]
 for L in open(0).read().split(','):
  A,B=map(int,L.split('-'))
- for i in 2,3,5,7:
-  t={(j:=int(str(J)*i))*(A<=j<=B)for J in range(int(B**(1/i)))}
-  n+=sum(t)*(i==2)
-  S|=t
-print(n,sum(S))
+ for i in 2,3,5,7:Q[i%2]|={(j:=int(str(J)*i))*(A<=j<=B)for J in range(int(B**(1/i)))}
+print(sum(S),sum(S|T))
