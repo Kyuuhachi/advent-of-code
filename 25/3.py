4459b5d578f7a8cbfeb757efeeb1fef551e986aa
Kyuuhachi <caagr98@gmail.com>
2025-12-03 08:11:56 +0100

    145

--- 25/3.py
+++ 25/3.py
@@ -1,4 +1,4 @@
 n=[0,0]
 for L in open(0):
- for M in[2,12]:i=-1;n[M>6]+=int(''.join(L[i:=max(range(i+1,len(L)-M+k),key=lambda i:L[i])]for k in range(M)))
+ for M in 2,12:i=-1;n[M>6]+=int(''.join(L[i:=max(range(i+1,len(L)-M+k),key=lambda i:L[i])]for k in range(M)))
 print(n)
