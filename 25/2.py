965e630c643cfb1833c3d589f224965222fab21f
Kyuuhachi <caagr98@gmail.com>
2025-12-03 19:22:49 +0100

    197

--- 25/2.py
+++ 25/2.py
@@ -2,9 +2,7 @@ S={n:=0}
 for L in open(0).read().split(','):
  A,B=map(int,L.split('-'))
  for i in 2,3,5,7:
-  for j in range(int(B**(1/i))):
-   j=int(str(j)*i)
-   j*=A<=j<=B
-   n+=j*(i==2)
-   S|={j}
+  t={(j:=int(str(J)*i))*(A<=j<=B)for J in range(int(B**(1/i)))}
+  n+=sum(t)*(i==2)
+  S|=t
 print(n,sum(S))
