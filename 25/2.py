0bc9010c1bd4f45c3da2f26a28bc086e69f9cf7b
Kyuuhachi <caagr98@gmail.com>
2025-12-03 19:09:29 +0100

    198

--- 25/2.py
+++ 25/2.py
@@ -1,7 +1,7 @@
 S={n:=0}
 for L in open(0).read().split(','):
  A,B=map(int,L.split('-'))
- for i in[2,3,5,7]:
+ for i in 2,3,5,7:
   for j in range(int(B**(1/i))):
    j=int(str(j)*i)
    j*=A<=j<=B
