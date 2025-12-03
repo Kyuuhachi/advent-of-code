51e48043cd1c5596802427a2897a090915aefec7
Kyuuhachi <caagr98@gmail.com>
2025-12-03 07:58:39 +0100

    169

--- 25/3.py
+++ 25/3.py
@@ -1,8 +1,8 @@
 def F(I,M,i=0):
- for k in range(M,0,-1):
-  i=max(range(i,len(L)-k),key=lambda i:L[i])
-  n[I]+=int(L[i]+'0'*(k-1))
+ for k in range(M,-1,-1):
+  i=max(range(i,len(L)-k-1),key=lambda i:L[i])
+  n[I]+=int(L[i]+'0'*k)
   i+=1
 n=[0,0]
-for L in open(0):F(0,2);F(1,12)
+for L in open(0):F(0,1);F(1,11)
 print(n)
