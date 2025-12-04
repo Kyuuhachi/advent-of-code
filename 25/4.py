d1a158fa7a40dad7d46245ab738b9a0e5ee50021
Kyuuhachi <caagr98@gmail.com>
2025-12-04 08:30:19 +0100

    218

--- 25/4.py
+++ 25/4.py
@@ -3,10 +3,10 @@ R=range(len(i))
 W=i.find('\n')+1
 P={p for p in R if'?'<i[p]}
 n=[]
-while True:
+o=1
+while o:
  o={p for p in P if sum(p+t in P for t in(-W-1,-W,-W+1,-1,1,W-1,W,W+1))<4}
  n+=len(o),
  print(n)
  P-=o
- if not o:break
 print(n[0],sum(n))
