8cd521a33cc65206ba796fc6d22262415f20fd9f
Kyuuhachi <caagr98@gmail.com>
2025-12-04 08:33:17 +0100

    215

--- 25/4.py
+++ 25/4.py
@@ -5,7 +5,7 @@ P={p for p in R if'?'<i[p]}
 n=[]
 o=1
 while o:
- o={p for p in P if len(P&{p+t for t in(-W-1,-W,-W+1,-1,1,W-1,W,W+1)})<4}
+ o={p for p in P if len(P&{p-W-1,p-W,p-W+1,p-1,p+1,p+W-1,p+W,p+W+1})<4}
  n+=len(o),
  print(n)
  P-=o
