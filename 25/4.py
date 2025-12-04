badabe64a48618fa2363794f0ab37c12fdaa8523
Kyuuhachi <caagr98@gmail.com>
2025-12-04 08:29:51 +0100

    4b, 233

--- 25/4.py
+++ 25/4.py
@@ -2,9 +2,11 @@ i=open(0).read()
 R=range(len(i))
 W=i.find('\n')+1
 P={p for p in R if'?'<i[p]}
-n=0
-for p in R:
- b=sum(p+t in P for t in(-W-1,-W,-W+1,-1,1,W-1,W,W+1))<4*(p in P)
- n+=b
- print('x'if b else i[p],end='')
-print(n)
+n=[]
+while True:
+ o={p for p in P if sum(p+t in P for t in(-W-1,-W,-W+1,-1,1,W-1,W,W+1))<4}
+ n+=len(o),
+ print(n)
+ P-=o
+ if not o:break
+print(n[0],sum(n))
