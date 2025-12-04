4d83c548e3eca8ce7610c9c1e963cc51d57a782c
Kyuuhachi <caagr98@gmail.com>
2025-12-04 08:46:57 +0100

    189

--- 25/4.py
+++ 25/4.py
@@ -2,7 +2,5 @@ i=open(0).read()
 W=i.find('\n')
 P={p for p in range(len(i))if'?'<i[p]}
 n=[]
-while o:={p for p in P if len(P&{p-W-2,p-W-1,p-W,p-1,p+1,p+W,p+W+1,p+W+2})<4}:
- n+=len(o),
- P-=o
+while o:={p for p in P if len(P&{p-W-2,p-W-1,p-W,p-1,p+1,p+W,p+W+1,p+W+2})<4}:n+=len(o),;P-=o
 print(n[0],sum(n))
