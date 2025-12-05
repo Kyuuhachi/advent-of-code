380fc60997f49e888b8046b0e3fcfa3fc51b854a
Kyuuhachi <caagr98@gmail.com>
2025-12-05 08:21:05 +0100

    193

--- 25/5.py
+++ 25/5.py
@@ -3,9 +3,9 @@ for L in(I:=open(0)):
  if'.'>L:break
  A,B=map(int,L.split('-'))
  R+=(A,B+1),
+*I,=I
 R.sort()
 n=m=0
-for L in I:n+=any(A<=int(L)<B for A,B in R)
 P=0
-for A,B in R:m-=max(A,P)-(P:=max(B,P))
+for A,B in R:m-=(K:=max(A,P))-(P:=max(B,P));n+=sum(K<=int(i)<P for i in I)
 print(n,m)
