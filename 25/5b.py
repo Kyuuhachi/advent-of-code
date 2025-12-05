13d29f1a18ce3cc51de686f22f62b1082c7e3766
Kyuuhachi <caagr98@gmail.com>
2025-12-05 18:54:10 +0100

    181

--- 25/5b.py
+++ 25/5b.py
@@ -1,9 +1,8 @@
 E=[]
 for L in open(0):E+=zip(map(int,L.split('-')),[-1,1])if'-'in L else[(int('0'+L),0)]
-n=m=d=0
+n=d=0
 for V,D in sorted(E):
  d+=D<0
- n+=(D==0)*(d>0)
- m+=(V+(D>0))*D*(d==1)
+ n+=(D==0)*(d>0)+(V+(D>0))*D*(d==1)*1j
  d-=D>0
-print(n,m)
+print(n)
