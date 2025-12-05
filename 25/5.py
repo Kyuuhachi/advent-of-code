bc1f2544396694377f362978848241a9a1624cdd
Kyuuhachi <caagr98@gmail.com>
2025-12-05 08:36:00 +0100

    179

--- 25/5.py
+++ 25/5.py
@@ -3,7 +3,6 @@ for L in(I:=open(0)):
  if'.'>L:break
  R+=[*map(int,L.split('-'))],
 *I,=I
-n=0
-P=0
+n=P=0
 for A,B in sorted(R):K,P=max(A,P),max(B+1,P);n-=sum(K<=int(i)<P for i in I)*1j+K-P
 print(n*1j)
