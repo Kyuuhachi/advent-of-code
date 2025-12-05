3d127cddea0a805aaff649d0ddde6b7cb1cf8258
Kyuuhachi <caagr98@gmail.com>
2025-12-05 19:08:04 +0100

    158

--- 25/5b.py
+++ 25/5b.py
@@ -1,5 +1,5 @@
 E=[]
 for L in open(0):E+=zip(map(int,('0'+L).split('-')),[-('-'in L),1])
 n=d=0
-for V,D in sorted(E):d+=D<0;n+=(D==0)*(d>0)+(V+(D>0))*D*(d==1)*1j;d-=D>0
+for V,D in sorted(E):d-=D;n+=(D==0)*(d>0)+(V+(D>0))*D*(d+(D>0)==1)*1j
 print(n)
