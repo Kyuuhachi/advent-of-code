949764ca54198a7b596e4831b15b466d1fff2056
Kyuuhachi <caagr98@gmail.com>
2025-12-05 08:22:37 +0100

    192

--- 25/5.py
+++ 25/5.py
@@ -4,8 +4,7 @@ for L in(I:=open(0)):
  A,B=map(int,L.split('-'))
  R+=(A,B+1),
 *I,=I
-R.sort()
 n=m=0
 P=0
-for A,B in R:m-=(K:=max(A,P))-(P:=max(B,P));n+=sum(K<=int(i)<P for i in I)
+for A,B in sorted(R):m-=(K:=max(A,P))-(P:=max(B,P));n+=sum(K<=int(i)<P for i in I)
 print(n,m)
