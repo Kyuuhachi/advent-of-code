e5c5a52f9865c930aa5b393a300ef087f15c32ae
Kyuuhachi <caagr98@gmail.com>
2025-12-05 08:14:58 +0100

    205

---
+++ 25/5.py
@@ -0,0 +1,13 @@
+R=[]
+for L in(I:=open(0)):
+ if L<'.':break
+ A,B=map(int,L.split('-'))
+ R+=(A,B+1),
+R.sort()
+n=m=0
+for L in I:n+=any(A<=int(L)<B for A,B in R)
+P=0
+for A,B in R:
+ m+=max(B,P)-max(A,P)
+ P=max(B,P)
+print(n,m)
