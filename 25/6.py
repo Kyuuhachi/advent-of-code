90aae8d2ff6c0b3156b311f85ece3a056aeebc98
Kyuuhachi <caagr98@gmail.com>
2025-12-06 09:39:18 +0100

    208

--- 25/6.py
+++ 25/6.py
@@ -1,6 +1,12 @@
 *I,=open(0)
 *A,=zip(*[L.split()for L in I])
-n=0
-for Q in A:
- n+=eval(Q[-1].join(Q[:-1]))
-print(n)
+B=map(''.join,zip(*I))
+n=sum(eval(Q[-1].join(Q[:-1]))for Q in A)
+
+m=0
+for*L,O in B:
+ for Q in B:
+  if not Q.strip():break
+  L+=O,*Q
+ m+=eval("".join(L))
+print(n,m)
