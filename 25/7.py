47fa2f2f233ee2cab5bdca4b54ad2801323ae3c1
Kyuuhachi <caagr98@gmail.com>
2025-12-07 09:14:13 +0100

    181

--- 25/7.py
+++ 25/7.py
@@ -5,6 +5,6 @@ n=0
 for L in I:
  S2=[*S]
  for i in range(len(L)):
-  if'^'==L[i]:S2[i-1]+=S[i];S2[i+1]+=S[i];S2[i]-=S[i];n+=S[i]>0
+  if'^'==L[i]:s=S[i];S2[i-1]+=s;S2[i+1]+=s;S2[i]-=s;n+=s>0
  S=S2
 print(n,sum(S))
