adffa249b8449ef9289723841fa8f26cb9af4770
Kyuuhachi <caagr98@gmail.com>
2025-12-07 09:13:23 +0100

    186

--- 25/7.py
+++ 25/7.py
@@ -3,9 +3,8 @@ S=[0]*len(L)
 S[L.index('S')]=1
 n=0
 for L in I:
- S2=[0]*len(L)
+ S2=[*S]
  for i in range(len(L)):
-  if'^'==L[i]:S2[i-1]+=S[i];S2[i+1]+=S[i];n+=S[i]>0
-  else:S2[i]+=S[i]
+  if'^'==L[i]:S2[i-1]+=S[i];S2[i+1]+=S[i];S2[i]-=S[i];n+=S[i]>0
  S=S2
 print(n,sum(S))
