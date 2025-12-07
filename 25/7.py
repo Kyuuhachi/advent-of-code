f6c9d94fe028150b3be58c42c4e5a2703ee36422
Kyuuhachi <caagr98@gmail.com>
2025-12-07 13:13:22 +0100

    128

--- 25/7.py
+++ 25/7.py
@@ -1,4 +1,5 @@
 S=[n:=0]*999
 for L in open(0):
- for i in range(len(L)):s=S[i]*('.'<L[i]);S[i-1]+=s;S[i+1]+=s;S[i]-=s-(L[i]=='S');n+=s>0
+ i=0
+ for c in L:s=S[i]*('.'<c);S[i-1]+=s;S[i+1]+=s;S[i]-=s-(c=='S');n+=s>0;i+=1
 print(n,sum(S))
