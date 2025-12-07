280eb32c4e26ad33caba9dc62577609dd65ae6a6
Kyuuhachi <caagr98@gmail.com>
2025-12-07 09:16:39 +0100

    174

--- 25/7.py
+++ 25/7.py
@@ -4,8 +4,8 @@ S=[0]*N
 S[L.index('S')]=1
 n=0
 for L in I:
- S2=[*S]
+ Z=[*S]
  for i in range(N):
-  if'^'==L[i]:s=S[i];S2[i-1]+=s;S2[i+1]+=s;S2[i]=0;n+=s>0
- S=S2
+  if'^'==L[i]:s=S[i];Z[i-1]+=s;Z[i+1]+=s;Z[i]=0;n+=s>0
+ S=Z
 print(n,sum(S))
