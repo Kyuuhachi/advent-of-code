7609cbf03eaf5cffd7f24db6929a7d4a75f41421
Kyuuhachi <caagr98@gmail.com>
2025-12-07 09:12:12 +0100

    199

--- 25/7.py
+++ 25/7.py
@@ -1,7 +1,11 @@
-S,*I=[sum(1<<i for i,c in enumerate(L)if'.'<c)for L in open(0)]
-print(f"{S:032b}")
+L=next(I:=open(0))
+S=[0]*len(L)
+S[L.index('S')]=1
 n=0
 for L in I:
- n+=(L&S).bit_count()
- S=(S&~L)|(S&L)<<1|(S&L)>>1
- print(f"{S:032b}", n)
+ S2=[0]*len(L)
+ for i in range(len(L)):
+  if'^'==L[i]:S2[i-1]+=S[i];S2[i+1]+=S[i];n+=S[i]>0
+  else:S2[i]+=S[i]
+ S=S2
+print(n,sum(S))
