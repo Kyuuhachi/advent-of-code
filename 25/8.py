a1d0bc538304c7b64c136071f2b5882c5a0f8b77
Kyuuhachi <caagr98@gmail.com>
2025-12-08 08:19:31 +0100

    465

---
+++ 25/8.py
@@ -0,0 +1,28 @@
+E=enumerate
+S=[]
+for i,L in E(open(0)):
+ X,Y,Z=map(int,L.split(','))
+ S+=[i,X,Y,Z],
+D=sorted(
+ ((a-b)**2+(c-d)**2+(e-f)**2,i,j)
+ for i,a,c,e in S
+ for j,b,d,f in S
+ if i<j
+)
+class J:v=0;d=1
+V=[J()for _ in S]
+def R(v):
+ while v.v:v,v.v=v.v,v.v.v
+ return v
+for q,(_,i,j)in E(D):
+ A,B=R(V[i]),R(V[j])
+ if A!=B:
+  A.v=B
+  B.d+=A.d
+  if B.d==len(S):
+   print(S[i][1]*S[j][1])
+   break
+ if q==1000:
+  k=1
+  for v in sorted(A.d for A in V if not A.v)[-3:]:k*=v
+  print(k)
