eb726b0ac09ee03e4942207d848071fb1ea0d95b
Kyuuhachi <caagr98@gmail.com>
2025-12-08 13:01:42 +0100

    394, temp increase

--- 25/8.py
+++ 25/8.py
@@ -8,4 +8,4 @@ for _,i,j in Z((sum((a-b)**2 for a,b in zip(I,J)),i,j)for*I,i in S for*J,j in S[
  A,B=R(i),R(j);n+=1
  if A!=B:V[A].v=B;V[B].d+=V[A].d
  if V[B].d>T:print(k,S[i][0]*S[j][0]);break
- if n==T:*_,p,q,r=Z(A.d*(A.v<0)for A in V);k=p*q*r
+ if n==T:*_,p,q,r=Z(V[A].d*(V[A].v<0)for A in range(T));k=p*q*r
