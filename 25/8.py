ad62b18cedf7c3ad246f5942f31c05d40e5db68d
Kyuuhachi <caagr98@gmail.com>
2025-12-08 08:33:14 +0100

    425

--- 25/8.py
+++ 25/8.py
@@ -1,18 +1,17 @@
 S=[]
 for L in open(0):S+=[*map(int,L.split(',')),len(S)],
-D=sorted(
- (sum((a-b)**2 for a,b in zip(I,J)),i,j)
- for*I,i in S
- for*J,j in S
- if i<j
-)
 class J:v=0;d=1
 V=[J()for _ in S]
 def R(v):
  while v.v:v,v.v=v.v,v.v.v
  return v
 n=0
-for _,i,j in D:
+for _,i,j in sorted(
+ (sum((a-b)**2 for a,b in zip(I,J)),i,j)
+ for*I,i in S
+ for*J,j in S
+ if i<j
+):
  A,B=R(V[i]),R(V[j])
  if A!=B:
   A.v=B
