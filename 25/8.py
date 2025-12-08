ca7fa2972499c49ce73034996995a9bb2365636b
Kyuuhachi <caagr98@gmail.com>
2025-12-08 13:08:59 +0100

    355

--- 25/8.py
+++ 25/8.py
@@ -2,8 +2,7 @@ S=[]
 Z=sorted
 for L in open(n:=0):S+=[*map(int,L.split(',')),len(S)],
 T=len(S)
-D=[-1]*T
-V=[-1]*T
+*D,=V=[-1]*T
 R=lambda v:R(V[v])if-1<V[v]else v
 for _,i,j in Z((sum((a-b)**2 for a,b in zip(I,J)),i,j)for*I,i in S for*J,j in S[:i]):
  if n==T:*_,p,q,r=Z(d*v for d,v in zip(D,V));k=p*q*r
