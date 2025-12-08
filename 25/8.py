ed363b150eaba88e07444bf0f0dd653984ed0b77
Kyuuhachi <caagr98@gmail.com>
2025-12-08 12:34:32 +0100

    402

--- 25/8.py
+++ 25/8.py
@@ -7,9 +7,8 @@ def R(v):
  return v
 n=0
 for _,i,j in sorted((sum((a-b)**2 for a,b in zip(I,J)),i,j)for*I,i in S for*J,j in S if i<j):
- A,B=R(V[i]),R(V[j])
+ A,B=R(V[i]),R(V[j]);n+=1
  if A!=B:
   A.v=B;B.d+=A.d
   if B.d==len(S):print(k,S[i][0]*S[j][0]);break
- if n==1000:p,q,r=sorted(A.d for A in V if not A.v)[-3:];k=p*q*r
- n+=1
+ if n==999:p,q,r=sorted(A.d for A in V if not A.v)[-3:];k=p*q*r
