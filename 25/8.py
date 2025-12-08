6a10df95df7c49b318c93a0742dba901d878c09c
Kyuuhachi <caagr98@gmail.com>
2025-12-08 17:50:00 +0100

    344

--- 25/8.py
+++ 25/8.py
@@ -5,7 +5,7 @@ T=1000
 *D,=V=[-1]*T
 R=lambda v:R(V[v])if-1<V[v]else v
 for _,i,j in Z((sum((a-b)**2 for a,b in zip(I,J)),i,j)for*I,i in S for*J,j in S[:i]):
- if n==T:*_,p,q,r=Z(d*v for d,v in zip(D,V));k=p*q*r
+ if n==T:*_,p,q,r=Z(d*v for d,v in zip(D,V))
  A,B=R(i),R(j);n+=1
  if A!=B:V[A]=B;D[B]+=D[A]
- if-T==D[B]:-print(k,S[i][0]*S[j][0])
+ if-T==D[B]:-print(p*q*r,S[i][0]*S[j][0])
