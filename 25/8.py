81859d47e9ec918ba3c712e8fafec1550ec828d9
Kyuuhachi <caagr98@gmail.com>
2025-12-08 12:53:19 +0100

    367

--- 25/8.py
+++ 25/8.py
@@ -1,10 +1,10 @@
 S=[]
-for L in open(n:=0):S+=[*map(int,L.split(',')),len(S)],
+for L in open(n:=0):S+=[*map(int,L.split(',')),T:=len(S)],
 class J:v=0;d=1
 V=[J()for _ in S]
 R=lambda v:v.v and R(v.v)or v
 for _,i,j in sorted((sum((a-b)**2 for a,b in zip(I,J)),i,j)for*I,i in S for*J,j in S[:i]):
  A,B=R(V[i]),R(V[j]);n+=1
  if A!=B:A.v=B;B.d+=A.d
- if B.d==len(S):print(k,S[i][0]*S[j][0]);break
- if n==999:*_,p,q,r=sorted(A.d*(A.v==0)for A in V);k=p*q*r
+ if B.d>T:print(k,S[i][0]*S[j][0]);break
+ if n==T:*_,p,q,r=sorted(A.d*(A.v==0)for A in V);k=p*q*r
