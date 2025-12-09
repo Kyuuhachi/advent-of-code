f46e9660f9c5508294871f1111e10419d203768e
Kyuuhachi <caagr98@gmail.com>
2025-12-09 23:53:41 +0100

    513

--- 25/9.py
+++ 25/9.py
@@ -10,7 +10,7 @@ def I(A,B,C,D,E,F,G,H):
  else:e,f=Z((A,C));return c<B<d and max(e,a)<min(f,b)
 
 m = 0
-R=Z(zip(P[-1:]+P,P,P[1:]+P))
+R=Z(zip(P,P[1:]+P,P[2:]+P))
 for A,B,C in R:
  for D in P:
   if m<(s:=X(*B,*D))and Z((O(*A,*B,*D),O(*B,*C,*D)))[O(*A,*B,*C)>0]>0 and 1-any(I(*a,*b,*B,*D)for a,b,_ in R):m=s
