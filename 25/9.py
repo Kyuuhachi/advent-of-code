648f7cab951255025d5b15a0d5737cbf92317f4c
Kyuuhachi <caagr98@gmail.com>
2025-12-10 00:21:15 +0100

    466

--- 25/9.py
+++ 25/9.py
@@ -1,6 +1,6 @@
 P=[eval(x)for x in open(0)]
 X=lambda a,c,b,d:~abs(a-b)*~abs(c-d)
-print(max(X(*A,*B)for A in P for B in P))
+print(max(X(*A+B)for A in P for B in P))
 Z=sorted
 O=lambda A,B,C,D,E,F:(C-A)*(F-B)-(D-B)*(E-A)
 Y=lambda A,B,C,D:Z((A,C))+Z((B,D))
@@ -9,5 +9,5 @@ m=0
 R=Z(zip(P,P[1:]+P,P[2:]+P))
 for A,B,C in R:
  for D in P:
-  if m<(s:=X(*B,*D))and Z((O(*A,*B,*D),O(*B,*C,*D)))[O(*A,*B,*C)>0]>0 and 1-any(I(*Y(*B+D),*Y(*a+b))for a,b,_ in R):m=s
+  if m<(s:=X(*B+D))and Z((O(*A+B+D),O(*B+C+D)))[O(*A+B+C)>0]>0 and 1-any(I(*Y(*B+D)+Y(*a+b))for a,b,_ in R):m=s
 print(m)
