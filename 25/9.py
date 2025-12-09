ec071df3282056ab003bda58780ab504c1ebc89c
Kyuuhachi <caagr98@gmail.com>
2025-12-10 00:20:10 +0100

    475

--- 25/9.py
+++ 25/9.py
@@ -4,7 +4,7 @@ print(max(X(*A,*B)for A in P for B in P))
 Z=sorted
 O=lambda A,B,C,D,E,F:(C-A)*(F-B)-(D-B)*(E-A)
 Y=lambda A,B,C,D:Z((A,C))+Z((B,D))
-def I(a,c,b,d,e,g,f,h):return[a<e<c,b<f<d][f==h]&[max(f,b)<min(h,d),max(e,a)<min(g,c)][f==h]
+I=lambda a,c,b,d,e,g,f,h:[a<e<c,b<f<d][f==h]&[max(f,b)<min(h,d),max(e,a)<min(g,c)][f==h]
 m=0
 R=Z(zip(P,P[1:]+P,P[2:]+P))
 for A,B,C in R:
