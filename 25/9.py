920d257d6bd577d5bb18fb4f3aa80a9e2e4a8258
Kyuuhachi <caagr98@gmail.com>
2025-12-09 22:50:02 +0100

    Remove svg

--- 25/9.py
+++ 25/9.py
@@ -1,10 +1,5 @@
 P=[eval(x)for x in open(0)]
 print(max(~(a-b)*~(c-d)for a,c in P for b,d in P))
-s="""<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 20 20" stroke-width="0.2">\n"""
-s += f"""<rect width="20" height="20" fill="#777777"/>\n"""
-s += f"""<path d="M{" ".join(f"{x},{y}"for x,y in P)}Z" fill="none" stroke="black"/>\n"""
-for p in P:
- s += f"""<circle cx="{p[0]}" cy="{p[1]}" r="0.2" fill="black"/>\n"""
 
 def orient(a, b, c):
  return (b[0] - a[0])*(c[1] - a[1]) - (b[1] - a[1])*(c[0] - a[0])
@@ -48,11 +43,6 @@ for (x0,y0),(x,y),(x2,y2) in zip(P[-1:]+P,P,P[1:]+P):
  for ox, oy in P:
   # if ox==x or oy==y:continue
   c = check()
-  colors = ["#FF0000","#0000FF","#00FF00"]
   if c==2:
    m = max(m, ~abs(x-ox)*~abs(y-oy))
-
-  s += f"""<line x1="{x}" y1="{y}" x2="{(3*x+ox)/4}" y2="{(3*y+oy)/4}" stroke="{colors[c]}" stroke-width="0.1" style="mix-blend-mode:screen"/>\n"""
-s += "</svg>"
 print(m)
-open("9.svg","w").write(s)
