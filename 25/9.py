5dbf81697e1bf95b54c044991bb7608cf58312e3
Kyuuhachi <caagr98@gmail.com>
2025-12-10 00:48:36 +0100

    Revert "409"
    This reverts commit 36cb8b3a513f595462ba07886fb7521224f9abfe.

--- 25/9.py
+++ 25/9.py
@@ -8,5 +8,5 @@ R=Z(zip(P,P[1:]+P,P[2:]+P))
 for A,B,C in R:
  for D in P:
   V=a,c,b,d=B+D;n=max(n,s:=~abs(a-b)*~abs(c-d))
-  if s-m>0<Z((O(*A+V),O(*C+V)))[O(*A+B+C)>0]and 1-any(I(*Y(*V)+Y(*a+b))for a,b,_ in R):m=s
+  if s-m>0<Z((O(*A+V),O(*B+C+D)))[O(*A+B+C)>0]and 1-any(I(*Y(*V)+Y(*a+b))for a,b,_ in R):m=s
 print(n,m)
