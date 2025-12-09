64d8ca70454f1b967d90fb7f703910f9ef78edcd
Kyuuhachi <caagr98@gmail.com>
2025-12-10 00:30:20 +0100

    442

--- 25/9.py
+++ 25/9.py
@@ -4,9 +4,10 @@ Z=sorted
 O=lambda A,B,C,D,E,F:(C-A)*(F-B)-(D-B)*(E-A)
 Y=lambda A,B,C,D:Z((A,C))+Z((B,D))
 I=lambda A,B,C,D,E,F,G,H:[A<E<B,C<G<D][G==H]&[max(G,C)<min(H,D),max(E,A)<min(F,B)][G==H]
-m=0
+n=m=0
 R=Z(zip(P,P[1:]+P,P[2:]+P))
 for A,B,C in R:
  for D in P:
   if m<(s:=X(*B+D))and Z((O(*A+B+D),O(*B+C+D)))[O(*A+B+C)>0]>0 and 1-any(I(*Y(*B+D)+Y(*a+b))for a,b,_ in R):m=s
-print(max(X(*A+B)for A in P for B in P),m)
+  n=max(n,s)
+print(n,m)
