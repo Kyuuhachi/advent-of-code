2b1a0b50573b779b1eacdf46d9dcbaf3d7075f98
Kyuuhachi <caagr98@gmail.com>
2025-12-10 00:08:34 +0100

    This order of parameters makes more sense

--- 25/9.py
+++ 25/9.py
@@ -3,10 +3,10 @@ X=lambda a,c,b,d:~abs(a-b)*~abs(c-d)
 print(max(X(*A,*B)for A in P for B in P))
 Z=sorted
 O=lambda A,B,C,D,E,F:(C-A)*(F-B)-(D-B)*(E-A)
-def I(A,B,C,D,E,F,G,H):a,b=Z((E,G));c,d=Z((F,H));e,f=Z((B,D));g,h=Z((A,C));return a<A<b and max(e,c)<min(f,d)if B-D else c<B<d and max(g,a)<min(h,b)
+def I(A,B,C,D,E,F,G,H):a,b=Z((A,C));c,d=Z((B,D));e,f=Z((F,H));g,h=Z((E,G));return a<E<b and max(e,c)<min(f,d)if F-H else c<F<d and max(g,a)<min(h,b)
 m=0
 R=Z(zip(P,P[1:]+P,P[2:]+P))
 for A,B,C in R:
  for D in P:
-  if m<(s:=X(*B,*D))and Z((O(*A,*B,*D),O(*B,*C,*D)))[O(*A,*B,*C)>0]>0 and 1-any(I(*a,*b,*B,*D)for a,b,_ in R):m=s
+  if m<(s:=X(*B,*D))and Z((O(*A,*B,*D),O(*B,*C,*D)))[O(*A,*B,*C)>0]>0 and 1-any(I(*B,*D,*a,*b)for a,b,_ in R):m=s
 print(m)
