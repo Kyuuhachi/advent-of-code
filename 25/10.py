e1a9e7f0c7e5a6e68f65cb9fe60dfe28e4d23349
Kyuuhachi <caagr98@gmail.com>
2025-12-10 08:21:41 +0100

    Use sets instead of bits

--- 25/10.py
+++ 25/10.py
@@ -1,11 +1,12 @@
 for L in open(n:=0):
  A,*B,C=L.split()
- A=sum(1<<i-1 for i,c in enumerate(A)if c=='#')
- B=[sum(1<<n for n in eval(x.replace(*"({").replace(*")}")))for x in B]
- print(len(B))
+ A={i-1 for i,c in enumerate(A)if c=='#'}
+ B=[{int(n)for n in x[1:-1].split(",")}for x in B]
+ C=[int(n)for n in C[1:-1].split(",")]
+ print(A,B,C)
  w=1e9
  for b in range(1<<len(B)):
-  d=0
+  d=A^A
   for i in range(len(B)):
    if b>>i&1:d^=B[i]
   if d==A:w=min(w,b.bit_count())
