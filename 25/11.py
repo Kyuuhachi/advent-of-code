c6d6e4b0144c9d235f14efe52959cea17249166e
Kyuuhachi <caagr98@gmail.com>
2025-12-11 08:33:00 +0100

    255

--- 25/11.py
+++ 25/11.py
@@ -1,11 +1,10 @@
 G={}
 for L in open(0):A,*B=L.split();G[A[:-1]]=B
 O={"out":[1,0,0,0]}
-while G:
- for K in[*G]:
-  A=B=C=D=0
-  try:
-   for V in G[K]:p,q=V=="fft",V=="dac";a,b,c,d=O[V];A+=a;B+=b+a*p;C+=c+a*q;D+=d+c*p+b*q
-   O[K]=A,B,C,D;del G[K]
-  except:0
+for K in[*G]*99:
+ A=B=C=D=0
+ try:
+  for V in G[K]:p,q=V=="fft",V=="dac";a,b,c,d=O[V];A+=a;B+=b+a*p;C+=c+a*q;D+=d+c*p+b*q
+  O[K]=A,B,C,D;del G[K]
+ except:0
 print(O["you"][0],O["svr"][3])
