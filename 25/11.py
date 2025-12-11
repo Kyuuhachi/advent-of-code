ce134fe60dde14d58e45abb3fa8ce33e7ed8a6ff
Kyuuhachi <caagr98@gmail.com>
2025-12-11 08:26:05 +0100

    283

--- 25/11.py
+++ 25/11.py
@@ -5,9 +5,9 @@ for L in open(0):
 O={"out":[1,0,0,0]}
 while G:
  for K in[*G]:
-  N=[0]*4
+  A=B=C=D=0
   for V in G[K]:
-   if V in O:p,q=V=="fft",V=="dac";a,b,c,d=O[V];N[0]+=a;N[1]+=b+a*p;N[2]+=c+a*q;N[3]+=d+c*p+b*q
+   if V in O:p,q=V=="fft",V=="dac";a,b,c,d=O[V];A+=a;B+=b+a*p;C+=c+a*q;D+=d+c*p+b*q
    else:break
-  else:O[K]=N;del G[K]
+  else:O[K]=A,B,C,D;del G[K]
 print(O["you"][0],O["svr"][3])
