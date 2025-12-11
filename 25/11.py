253aae24b829e205e34d22292459fd1004af22f2
Kyuuhachi <caagr98@gmail.com>
2025-12-11 17:33:51 +0100

    218, differnt approach

--- 25/11.py
+++ 25/11.py
@@ -1,9 +1,5 @@
 G={L[:3]:L[4:].split()for L in open(0)}
-O={"out":[1,0,0,0]}
-for K in[*G]*99:
- A=B=C=D=0
- try:
-  for V in G[K]:p,q=V=="fft",V=="dac";a,b,c,d=O[V];A+=a;B+=b+a*p;C+=c+a*q;D+=d+c*p+b*q
-  O[K]=A,B,C,D;del G[K]
- except:0
-print(O["you"][0],O["svr"][3])
+from functools import*
+R=cache(lambda a,b:(a==b)+sum(R(c,b)for c in G.get(a,())))
+A,B,C,D,E="you out svr fft dac".split()
+print(R(A,B),R(C,D)*R(D,E)*R(E,B)+R(C,E)*R(E,D)*R(D,B))
