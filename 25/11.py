7c4831bd063cb73f6b01f34ae54754df855f502d
Kyuuhachi <caagr98@gmail.com>
2025-12-11 17:34:34 +0100

    203

--- 25/11.py
+++ 25/11.py
@@ -1,5 +1,4 @@
 G={L[:3]:L[4:].split()for L in open(0)}
 from functools import*
-R=cache(lambda a,b:(a==b)+sum(R(c,b)for c in G.get(a,())))
-A,B,C,D,E="you out svr fft dac".split()
-print(R(A,B),R(C,D)*R(D,E)*R(E,B)+R(C,E)*R(E,D)*R(D,B))
+R=cache(lambda a,b="out":(a==b)+sum(R(c,b)for c in G.get(a,())))
+print(R("you"),R(C:="svr",D:="fft")*R(D,E:="dac")*R(E)+R(C,E)*R(E,D)*R(D))
